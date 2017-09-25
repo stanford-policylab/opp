0167ecbf7cb81abecf8494c86ce7675f7e8d35fc
Daniel Jenson <daniel.a.jenson@gmail.com>
2017-09-25 12:47:07 -0700

    adding .gitignore, testing scratch

--- scratch.R
+++ scratch.R
@@ -2,10 +2,12 @@ library(tidyverse)
 library(lubridate)
 
 # TODO(danj): figure out how to pass column names not as strings
-plot_top_n_by_year <- function(tbl, top_n, date_col, .col_to_rank) {
+plot_top_n_by_year <- function(tbl, date_col, col_to_rank, top_n = 10) {
+  stopifnot(is.data.frame(tbl) || is.list(tbl) || is.environment(tbl))
+
   d <- tbl %>%
-    mutate(yr = year(date_col)) %>%
-    group_by(yr, col_to_rank) %>%
+    mutate(yr = year(substitute(date_col))) %>%
+    group_by(yr, substitute(col_to_rank)) %>%
     count() %>%
     group_by(yr) %>%
     mutate(yr_rank = row_number(-n)) %>%
@@ -13,7 +15,7 @@ plot_top_n_by_year <- function(tbl, top_n, date_col, .col_to_rank) {
     arrange(yr, yr_rank)
 
   ggplot(d) +
-    geom_bar(aes(x = col_to_rank, y = n), stat = "identity") +
+    geom_bar(aes(x = substitute(col_to_rank), y = n), stat = "identity") +
     facet_grid(yr ~ .) +
     theme(axis.text.x = element_text(angle = 90, hjust = 1))
 }
