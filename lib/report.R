113a459a505015c79ed72cd64e7ba960db83d238
Daniel Jenson <daniel.a.jenson@gmail.com>
2017-11-29 14:39:50 -0800

    progress

--- lib/report.R
+++ lib/report.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(zoo)
 
 source("opp.R")
 
@@ -6,14 +7,17 @@ d <- opp_load(state, city)
 
 total_rows <- nrow(d)
 
-by_type <- group_by(d, incident_type) %>% count
-by_type_plot <- ggplot(by_type) +
-  geom_bar(aes(x = factor(type), y = n), stat = "identity") +
-  xlab("type") +
-  ylab("count")
+by_incident_type <- group_by(d, incident_type) %>% count
+by_incident_type_table <- kable(by_incident_type)
 
 by_year <- group_by(d, year = year(incident_date)) %>% count
 by_year_plot <- ggplot(by_year) +
   geom_bar(aes(x = factor(year), y = n), stat = "identity") +
   xlab("year") +
+  ylab("count")
+
+by_year_by_month_plot <- ggplot(d) +
+  geom_bar(aes(as.yearmon(incident_date))) +
+  scale_x_yearmon() +
+  xlab("time") +
   ylab("count")
