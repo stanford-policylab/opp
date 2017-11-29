6ab66b2ca68397e09893f72d4e3202bd81d88562
Daniel Jenson <daniel.a.jenson@gmail.com>
2017-11-29 14:59:56 -0800

    progress

--- lib/report.R
+++ lib/report.R
@@ -6,6 +6,7 @@ source("opp.R")
 d <- opp_load(state, city)
 
 total_rows <- nrow(d)
+date_range <- range(d$incident_date)
 
 by_incident_type <- group_by(d, incident_type) %>% count
 by_incident_type_table <- kable(by_incident_type)
@@ -19,5 +20,11 @@ by_year_plot <- ggplot(by_year) +
 by_year_by_month_plot <- ggplot(d) +
   geom_bar(aes(as.yearmon(incident_date))) +
   scale_x_yearmon() +
-  xlab("time") +
+  xlab("month-year") +
+  ylab("count")
+
+by_year_by_day_plot <- ggplot(d) +
+  geom_histogram(aes(x = yday(incident_date)), bins = 365) + 
+  facet_grid(year(incident_date) ~ .) +
+  xlab("day of year") +
   ylab("count")
