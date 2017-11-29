4957ea6bdccd9b1ae1e33799e37bbabe33250f85
Daniel Jenson <daniel.a.jenson@gmail.com>
2017-11-29 14:09:26 -0800

    updating report

--- lib/report.R
+++ lib/report.R
@@ -1,11 +1,18 @@
-## @knitr load
+library(ggplot2)
+source("opp.R")
+
 d <- opp_load(state, city)
 
-## @knitr counts
-# nrows <- nrow(d)
-# by_type <- group_by(d, incident_type) %>% count
-# by_year <- group_by(d, year = year(incident_date)) %>% count
+nrows <- nrow(d)
 
-## @knitr part2
-print(city)
-print(state)
+by_type <- group_by(d, incident_type) %>% count
+by_type_plot <- ggplot(by_type) +
+  geom_bar(aes(x = factor(type), y = n), stat = "identity") +
+  xlab("type") +
+  ylab("count")
+
+by_year <- group_by(d, year = year(incident_date)) %>% count
+by_year_plot <- ggplot(by_year) +
+  geom_bar(aes(x = factor(year), y = n), stat = "identity") +
+  xlab("year") +
+  ylab("count")
