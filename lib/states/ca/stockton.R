b5a855e8f3a3c1f07c188b387e468c6dbea40da3
Daniel Jenson <Daniel Jenson>
2018-01-12 16:02:45 -0800

    progress

--- lib/states/ca/stockton.R
+++ lib/states/ca/stockton.R
@@ -8,19 +8,22 @@ load_raw <- function(raw_data_dir, geocodes_path) {
     loading_problems[[fname]] <<- problems(tbl)
     tbl
   }
+  # TODO(journalist): how do we join these sets of files? 
+  # stop_files contain date, time, location, officer demographics
+  stop_files <- c(
+    "jenna_fowler_013117_-_stocktonpd_cad_tstops_2012_2013_sheet_1.csv",
+    "jenna_fowler_013117_-_stocktonpd_cad_tstops__2014_july_2016_sheet_1.csv",
+    "jenna_fowler_013117_-_stocktonpd_tstops_aug_dec2016_sheet_1.csv"
+  )
+  # survey_files contain date, outcome, subject demographics
+  survey_files <- c(
+    "jenna_fowler_013117_-_stocktonpd_trafficstopsurvey_2012_july_2016_sheet_1.csv",
+    "jenna_fowler_013117_-_stocktonpd_trafficstopsurvey_aug_dec2016_sheet_1.csv"
+  )
+  data <- bind_rows(r(survey_files[1]), r(survey_files[2]))
 
-  data <- list()
-  f1 = "jenna_fowler_013117_-_stocktonpd_cad_tstops_2012_2013_sheet_1.csv"
-  f2 = "jenna_fowler_013117_-_stocktonpd_cad_tstops__2014_july_2016_sheet_1.csv"
-  f3 = "jenna_fowler_013117_-_stocktonpd_trafficstopsurvey_2012_july_2016_sheet_1.csv"
-  f4 = "jenna_fowler_013117_-_stocktonpd_trafficstopsurvey_aug_dec2016_sheet_1.csv"
-  f5 = "jenna_fowler_013117_-_stocktonpd_tstops_aug_dec2016_sheet_1.csv"
-  data[[f1]] <- r(f1)
-  data[[f2]] <- r(f2)
-  data[[f3]] <- r(f3)
-  data[[f4]] <- r(f4)
-  data[[f5]] <- r(f5)
-
+  # TODO(danj): location is in the stop files, but let's wait to
+  # geocode until we are sure we are going to use those files
   # data <- add_lat_lng(data, "address", geocodes_path)
 
 	list(data = data, metadata = list(loading_problems = loading_problems))
@@ -28,47 +31,59 @@ load_raw <- function(raw_data_dir, geocodes_path) {
 
 
 clean <- function(d) {
+  names(d$data) <- tolower(names(d$data))
+  tr_sex <- c(
+    Female = "female",
+    Male = "male"
+  )
+  tr_race <- c(
+    "Asian" = "asian/pacific islander",
+    "Black/African American" = "black",
+    "Hispanic" = "hispanic",
+    "Others" = "other/unknown",
+    "White/Caucasian" = "white"
+  )
+  # TODO(ravi): what to do about this outcome?
+  tr_outcome <- c(
+    "1-In-Custody Arrest" = "arrest",
+    "2-Citation Issued" = "citation",
+    "3-Verbal Warning" = "warning"
+    # 4-Public Service
+  )
+  # TODO(ravi): what should we classify these other search types as
+  tr_search_type <- c(
+    # 1-No Search Conducted
+    "2-Consent" = "consent",
+    "3-Probable Cause (Terry)" = "probable cause",
+    # 4-Two Inventory Search
+    "5-Incidental to Lawful Arrest" = "incident to arrest"
+    # 6-Pursuant to Lawful Search Warrant
+    # 7-Probation/Parole Search
+  )
+
   d$data %>%
     select(
-      -empty
+      -x3  # empty column
     ) %>%
     rename(
-      incident_id = rin,
-      incident_location = address,
-      # TODO(journalist): clarify relationship between
-      # mir_and_description, disposition_description, type
-      # https://app.asana.com/0/456927885748233/462732257741348 
-      reason_for_stop = mir_and_description
-    ) %>%
-    filter(
-      !is.na(incident_id)
-    ) %>%
-    separate_cols(
-      possible_race_and_sex = c("subject_race", "subject_sex"),
-      sep = 1
-    ) %>%
-    separate_cols(
-      datetime = c("incident_date", "incident_time"),
-      officer_no_name_1 = c("officer_id_1", "officer_name_1"),
-      officer_no_name_2 = c("officer_id_2", "officer_name_2")
+      incident_date = in_date,   
+      officer_id_1 = officer_id,
+      officer_id_2 = officer2_id,
+      subject_age = age,
+      subject_sex = gender,
+      subject_race = race,
+      probable_cause = probcause
     ) %>%
     mutate(
-      incident_type = "vehicular",
-      # NOTE: vehicular because mir_and_description are all traffic
-      incident_date = parse_date(incident_date, "%Y/%m/%d"),
-      incident_time = parse_time(incident_time),
-      subject_race = tr_race[subject_race],
+      incident_id = trafsurvey_id,
+      incident_outcome = tr_outcome[result],
+      search_type = tr_search_type[search],
       subject_sex = tr_sex[subject_sex],
-      arrest_made = str_sub(disposition_description, 1, 1) == "A",
-      # NOTE: includes criminal and non-criminal citations
-      citation_issued = matches(disposition_description, "CITATION"),
-      incident_outcome = first_of(
-        arrest = arrest_made,
-        citation = citation_issued
-      ),
-      subject_dob = ymd(subject_dob),
-      officer_id_1 = parse_number(officer_id_1),
-      officer_id_2 = parse_number(officer_id_2)
+      subject_race = tr_race[subject_race],
+      search_conducted = !startsWith(search, "1-No Search"),
+      arrest_made = result == "1-In-Custody Arrest",
+      citation_issued = result == "2-Citation Issued",
+      warning_issued = result == "3-Verbal Warning"
     ) %>%
     standardize(d$metadata)
 }
