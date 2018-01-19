07fc1a59cd90bcd2e63de9ee010125738e914ef8
Daniel Jenson <daniel.a.jenson@gmail.com>
2018-01-19 11:47:34 -0800

    progress

--- py-utils/text_dataset_generator.py
+++ py-utils/text_dataset_generator.py
@@ -5,18 +5,28 @@ import pandas as pd
 import sys
 
 
-def make_training_dataset(args):
-    df = pd.read_csv(args.csv_file, usecols=[args.column])
-    pass
+def make_training_dataset(csv_file, output_csv):
+    labels = []
+    with open(csv_file) as f:
+        for line in f:
+            print(line)
+            res = input('Label: ')
+            while (res not in args.labels):
+                res = input('Label: ')
+            labels.append(res)
+            texts.append(line)
+    df = pd.DataFrame({'label': labels, 'text': texts})
+    df.to_csv(output_csv, index=False)
+    print('output saved to %s!' % output_csv)
+    return
 
 
 def parse_args(argv):
     parser = argparse.ArgumentParser(prog=argv[0],
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('csv_file')
-    parser.add_argument('column')
+    parser.add_argument('csv_file', help='format (per line): "text"')
     parser.add_argument('labels', nargs='+')
-    parser.add_argument('-o', '--output_file', default='train.txt')
+    parser.add_argument('-o', '--output_csv', default='train.csv')
     return parser.parse_args(argv[1:])
 
 
