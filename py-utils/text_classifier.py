288e360ae79b69990cf8fe4e5b667a55c79e3418
Daniel Jenson <daniel.a.jenson@gmail.com>
2018-01-19 11:47:34 -0800

    progress

--- py-utils/text_classifier.py
+++ py-utils/text_classifier.py
@@ -10,38 +10,25 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import make_pipeline
 
 
-def train(train_file, model_name):
-    y = []
-    X = []
-    with open(train_file) as f:
-        for line in f:
-            label, text = line.split(' ', 1)
-            y.append(label)
-            X.append(text)
+def train(train_csv, model_name):
+    df = pd.read_csv(train_csv)
     # NOTE: stemming and punctuation?
     p = make_pipeline(CountVectorizer(analyzer='char',
                                       ngram_range=(2,4),
                                       stop_words=stopwords,
                                       lowercase=True),
                       MultinomialNB())
-    p.fit(X, y)
+    p.fit(df['text'], df['label'])
     joblib.dump(p, model_name)
     print('model saved as %s' % model_name)
     return
 
 
-def predict(model_file, test_file, output_file):
+def predict(model_file, test_csv, output_csv):
     m = joblib.load(model_file)
-    X = []
-    with open(test_file) as f:
-        for text in f:
-            X.append(text)
-    preds = m.predict(X)
-    assert len(preds) == len(X), 'Predictions and texts do not align!'
-    with open(output_file, 'w') as f:
-        f.write('label text\n')
-        for pred, text in zip(preds, X):
-            f.write(pred + ' ' + text + '\n')
+    df = pd.read_csv(test_csv)
+    df['pred'] = m.predict(X['text'])
+    df.to_csv(columns=['pred', 'text'])
     print('output written to %s' % output_file)
     return
 
@@ -50,20 +37,26 @@ def parse_args(argv):
     parser = argparse.ArgumentParser(prog=argv[0],
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     sub = parser.add_subparsers(help='commands', dest='command')
-    train = sub.add_parser('train', help='train a model')
-    train.add_argument('train_file', help='label text, 1 per line (space sep)',
-                       default='train.txt')
-    train.add_argument('model_name', help='model name', default='model')
-    pred = sub.add_parser('predict', help='predict on unlabeled data')
-    pred.add_argument('model_file', help='model file', default='model')
-    pred.add_argument('test_file', help='test file, 1 text per line')
-    pred.add_argument('output_file', help='output file', default='output.csv')
+    train = sub.add_parser('train', help='train a model',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    train.add_argument('-f', '--train_csv', default='train.csv',
+                       help='format (per line): label,"text"')
+    train.add_argument('-mn' , '--model_name', default='model',
+                       help='model name')
+    pred = sub.add_parser('predict', help='predict on unlabeled data',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    pred.add_argument('-f', '--test_csv', default='test.csv',
+                      help='format (per line): "text"')
+    pred.add_argument('-mn', '--model_name', default='model',
+                      help='model file')
+    pred.add_argument('-o', '--output_csv', default='output.csv',
+                      help='output csv')
     return parser.parse_args(argv[1:])
 
 
 if __name__ == '__main__':
     args = parse_args(sys.argv) 
     if args.command == 'train':
-        train(args.train_file, args.model_name)
+        train(args.train_csv, args.model_name)
     else:
-        predict(args.model_file, args.test_file, args.output_file)
+        predict(args.model_file, args.test_csv, args.output_csv)
