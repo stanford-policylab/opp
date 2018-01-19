7f372be25cc778e1cc432b862a23768c67d24ad1
Daniel Jenson <daniel.a.jenson@gmail.com>
2018-01-19 14:10:05 -0800

    progress

--- py-utils/text_dataset_generator.py
+++ py-utils/text_dataset_generator.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 
 import argparse
+import csv
 import os
 import pandas as pd
 import sys
 
 
-def make_training_dataset(csv_file, label_categories, sample_n, output_csv):
+def make_training_dataset(csv_file,
+                          label_categories,
+                          default_label,
+                          sample_n,
+                          output_csv):
     label_categories = set(label_categories)
+    if default_label:
+        assert default_label in label_categories, 'default label invalid!'
     labeled_texts = set()
     texts = set()
 
     if os.path.exists(output_csv):
-        df = pd.read_csv(output_csv)
+        df = pd.read_csv(output_csv, na_filter=False)
         all_labels = df['label']
         all_texts = df['text']
         labeled_texts = set(zip(all_labels, all_texts))
@@ -22,21 +29,32 @@ def make_training_dataset(csv_file, label_categories, sample_n, output_csv):
     count = 0
     with open(csv_file) as f:
         for text in f:
-            if text in current_texts:
+            text = text.strip()
+            if text in texts:
                 continue
-            print(text)
-            label = input('Label: ')
+            msg = text + ' : '
+            if default_label:
+                msg = text + ' (%s): ' % default_label
+            label = input(msg)
+            if not label and default_label:
+                label = default_label
             while (label not in label_categories):
-                label = input('Label: ')
+                label = input(msg)
             labeled_texts.add((label, text))
             texts.add(text)
             count += 1
             if count >= sample_n:
                 break
 
-    # TODO(danj): sort out writing
+    labels = []
+    texts = []
+    for label, text in labeled_texts:
+        labels.append(label)
+        texts.append(text)
+
     df = pd.DataFrame({'label': labels, 'text': texts})
-    df.to_csv(output_csv, index=False)
+    print(df)
+    df.to_csv(output_csv, index=False, quoting=csv.QUOTE_NONNUMERIC)
     print('output saved to %s!' % output_csv)
     return
 
@@ -46,6 +64,7 @@ def parse_args(argv):
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('source_csv', help='format (per line): "text"')
     parser.add_argument('label_categories', nargs='+')
+    parser.add_argument('-d', '--default_label')
     parser.add_argument('-n', '--sample_n', default=10,
                         help='how many values to sample')
     parser.add_argument('-o', '--output_csv', default='train.csv',
@@ -57,5 +76,6 @@ if __name__ == '__main__':
     args = parse_args(sys.argv) 
     make_training_dataset(args.source_csv,
                           args.label_categories,
+                          args.default_label,
                           args.sample_n,
                           args.output_csv)
