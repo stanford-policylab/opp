ea8d0198f4c61fda02a91957003264614903e80f
Daniel Jenson <daniel.a.jenson@gmail.com>
2018-01-30 15:54:43 -0800

    progress

--- py-utils/text_classifier.py
+++ py-utils/text_classifier.py
@@ -11,29 +11,35 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import make_pipeline
 
 
-def train(train_csv, model_name):
+def train_on(train_csv, model_name):
     df = pd.read_csv(train_csv, na_filter=False)
     label_cols = [col for col in df if col != 'text']
+    train(df.text, df[label_cols], model_name, label_cols)
+    return
+
+
+def train(X, y, save_model=None, label_names=None):
     # NOTE: stemming and punctuation?
     p = make_pipeline(CountVectorizer(analyzer='char_wb',
                                       ngram_range=(2,4),
                                       stop_words='english',
                                       lowercase=True),
                       RandomForestClassifier(n_estimators=200))
-    p.fit(df['text'], df[label_cols])
-    p.label_names = label_cols
-    joblib.dump(p, model_name)
-    print('model saved as %s' % model_name)
-    return
+    p.label_names = label_names
+    if y.shape[1] == 1:
+        y = y.iloc[:, 0].ravel()
+    p.fit(X, y)
+    if save_model:
+        joblib.dump(p, save_model)
+    return p
 
 
-def predict(model_file, test_csv, output_csv):
+def predict_on(model_file, test_csv, pred_csv):
     m = joblib.load(model_file)
     df = pd.read_csv(test_csv, names=['text'], na_filter=False)
     dfp = pd.DataFrame(m.predict(df['text']).astype(int),
                        columns=m.label_names)
-    dfp.join(df).to_csv(output_csv, index=False, quoting=csv.QUOTE_NONNUMERIC)
-    print('output written to %s' % output_csv)
+    dfp.join(df).to_csv(pred_csv, index=False, quoting=csv.QUOTE_NONNUMERIC)
     return
 
 
@@ -53,7 +59,7 @@ def parse_args(argv):
                       help='format (per line): "text"')
     pred.add_argument('-mn', '--model_name', default='model',
                       help='model file')
-    pred.add_argument('-o', '--output_csv', default='output.csv',
+    pred.add_argument('-o', '--pred_csv', default='pred.csv',
                       help='output csv')
     return parser.parse_args(argv[1:])
 
@@ -61,6 +67,6 @@ def parse_args(argv):
 if __name__ == '__main__':
     args = parse_args(sys.argv) 
     if args.command == 'train':
-        train(args.train_csv, args.model_name)
+        train_on(args.train_csv, args.model_name)
     else:
-        predict(args.model_name, args.test_csv, args.output_csv)
+        predict_on(args.model_name, args.test_csv, args.pred_csv)
