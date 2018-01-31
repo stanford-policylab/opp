c987e2eb737c71eba9df3792d710cb2da7079b62
Daniel Jenson <daniel.a.jenson@gmail.com>
2018-01-30 16:40:12 -0800

    progress

--- py-utils/text_classifier.py
+++ py-utils/text_classifier.py
@@ -37,8 +37,7 @@ def train(X, y, save_model=None, label_names=None):
 def predict_on(model_file, test_csv, pred_csv):
     m = joblib.load(model_file)
     df = pd.read_csv(test_csv, names=['text'], na_filter=False)
-    dfp = pd.DataFrame(m.predict(df['text']).astype(int),
-                       columns=m.label_names)
+    dfp = pd.DataFrame(m.predict(df['text']), columns=m.label_names)
     dfp.join(df).to_csv(pred_csv, index=False, quoting=csv.QUOTE_NONNUMERIC)
     return
 
@@ -51,14 +50,14 @@ def parse_args(argv):
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     train.add_argument('-f', '--train_csv', default='train.csv',
                        help='format (per line): label,"text"')
-    train.add_argument('-mn' , '--model_name', default='model',
+    train.add_argument('-m' , '--model_name', default='a.model',
                        help='model name')
     pred = sub.add_parser('predict', help='predict on unlabeled data',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    pred.add_argument('-m', '--model_name', default='a.model',
+                      help='model file')
     pred.add_argument('-f', '--test_csv', default='test.csv',
                       help='format (per line): "text"')
-    pred.add_argument('-mn', '--model_name', default='model',
-                      help='model file')
     pred.add_argument('-o', '--pred_csv', default='pred.csv',
                       help='output csv')
     return parser.parse_args(argv[1:])
