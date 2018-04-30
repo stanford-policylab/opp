b0a95ee5b8efff5d2b2aee1fedddf6f73077be99
Daniel Jenson <Daniel Jenson>
2018-04-30 13:39:56 -0700

    progress

---
+++ lib/states/ca/san_jose.R
@@ -0,0 +1,183 @@
+source("common.R")
+
+load_raw <- function(raw_data_dir, n_max) {
+  data <- tibble()
+  loading_problems <- list()
+  # NOTE: the first sheet of each contains the schema
+  load <- function(fname_prefix, n_sheets) {
+    for (i in 2:n_sheets) {
+      fname <- str_c(fname_prefix, "_sheet_", i, ".csv")
+      tbl <- read_csv(file.path(raw_data_dir, fname))
+      loading_problems[[fname]] <<- problems(tbl)
+      data <<- bind_rows(data, tbl)
+      if (nrow(data) > n_max) {
+        data <- data[1:n_max, ]
+        break
+      }
+    }
+  }
+  load("detentions_oct_14_to_aug_17", 36)
+  load("detentions_sept_13_to_sept_14", 15)
+  load("detentions_sept_17_to_march_18", 8)
+  bundle_raw(data, loading_problems)
+}
+
+
+clean <- function(d, calculated_features_path) {
+
+  tr_race <- c(
+    "H" = "hispanic",
+    "W" = "white",
+    "A" = "asian/pacific islander",
+    "B" = "black",
+    "O" = "other/unknown",
+    "V" = "other/unknown",
+    "S" = "other/unknown",
+    "P" = "other/unknown",
+    "N" = "other/unknown",
+    "I" = "other/unknown",
+    "C" = "other/unknown",
+    "F" = "other/unknown",
+    "Z" = "other/unknown",
+    "M" = "other/unknown",
+    "1" = "other/unknown",
+    "2" = "other/unknown",
+    "D" = "other/unknown",
+    "U" = "other/unknown",
+    "E" = "other/unknown",
+    "X" = "other/unknown",
+    "J" = "other/unknown",
+    "G" = "other/unknown",
+    "Q" = "other/unknown",
+    "0" = "other/unknown",
+    "3" = "other/unknown",
+    "5" = "other/unknown",
+    "L" = "other/unknown",
+    "4" = "other/unknown",
+    "K" = "other/unknown",
+    "." = "other/unknown",
+    "6" = "other/unknown",
+    "a" = "other/unknown",
+    "R" = "other/unknown",
+    "w" = "white",
+    "Y" = "other/unknown"
+  )
+
+  tr_search <- c(
+    "N" = FALSE, # no search
+    "S" = TRUE,  # search, contraband_found
+    "Z" = TRUE   # search, no contraband found 
+  )
+
+  tr_contraband <- c(
+    "S" = FALSE,  # search, contraband_found
+    "Z" = TRUE   # search, no contraband found 
+  )
+
+  tr_reason <- c(
+    "B" = "BOL/APB/Watch Bulletin",
+    "C" = "Consensual ",
+    "M" = "Muni Code Violation",
+    "P" = "Penal code, H&S, B&P violations, etc.",
+    "V" = "Vehicle Code Violation"
+  )
+
+  tr_event <- c(
+    "A" = "ARREST MADE",
+    "B" = "ARREST BY WARRANT",
+    "C" = "CRIMINAL CITATION",
+    "D" = "TRAFFIC CITATION ISSUED; HAZARDOUS VIOLATION",
+    "E" = "TRAFFIC CITATION ISSUED; NON-HAZARDOUS VIOLATION",
+    "F" = "FIELD INTERVIEW COMPLETED",
+    "G" = "GONE ON ARRIVAL/UNABLE TO LOCATE (N/A FOR CAR STOPS)",
+    "H" = "COURTESY SERVICE/CITIZEN OR AGENCY ASSIST",
+    "M" = "STRANDED MOTORIST ASSIST",
+    "N" = "NO REPORT REQUIRED; DISPATCH RECORD ONLY",
+    "O" = "REPORT, OTHER THAN PRIMARY REPORT, IS FILED",
+    "P" = "PRIOR CASE, FOLLOW-UP ACTIVITY ONLY",
+    "R" = "PRIMARY REPORT TAKEN (FILED)",
+    "T" = "TURNED OVER TO",
+    "U" = "UNFOUNDED EVENT"
+  )
+
+  vehicle_keywords <- str_c(
+    "CAR STOP",
+    "DRIV",
+    "DUI",
+    "HIT AND RUN",
+    "REGISTRATION",
+    "SPEED",
+    "VEHICLE",
+    sep = "|"
+  )
+
+  pedestrian_keywords <- str_c(
+    "DRUNK",
+    "FOOT PATROL",
+    "NARCOTICS",
+    "PEDESTRIAN",
+    "POSSESSION",
+    "PROSTITUTION",
+    "SUSPICIOUS PERSON",
+    "USE OF CONTROLLED SUBSTANCE",
+    sep = "|"
+  )
+
+  # TODO(ravi): this has interesting variables -- handcuffed as part of
+  # DETENTION TYPE / DETENTION REASON -- can we do anything with this?
+  # https://app.asana.com/0/456927885748233/649920459235534
+
+  # TODO(phoebe): can we get search_type?
+  # https://app.asana.com/0/456927885748233/649920459235535
+  d$data %>%
+    rename(
+      call_desc = `TYCOD DESCRIPTION`
+    ) %>%
+    filter(
+      str_detect(
+        call_desc,
+        str_c(vehicle_keywords, pedestrian_keywords, sep = "|")
+      ),
+      !str_detect(
+        call_desc,
+        str_c(
+          "STOLEN",
+          "ABANDONED",
+          "BURGLARY",
+          sep = "|"
+        )
+      )
+    ) %>%
+    mutate(
+      incident_type = ifelse(
+        str_detect(call_desc, vehicle_keywords),
+        "vehicular",
+        "pedestrian"
+      ),
+      incident_date = parse_date(`REPORT DATE`, "%Y%m%d"),
+      incident_time_period = seconds_to_period(`REPORT TIME (HMS)`),
+      incident_time = seconds_to_hms(`REPORT TIME (HMS)`),
+      # NOTE: COMMONPLACE seems to be a name for places with addresses,
+      # i.e. OVERFELT GARDENS, SAFEWAY, WALMART, etc...
+      incident_location = coalesce(
+        ADDRESS,
+        str_c(XSTREET1, XSTREET2, sep = " AND ")
+      ),
+      event_desc = tr_event[`EVENT DISPO`],
+      # TODO(ravi): should we filter out the other outcomes?
+      # https://app.asana.com/0/456927885748233/649920459235533
+      incident_outcome = first_of(
+        "arrest" = str_detect(event_desc, "ARREST"),
+        "citation" = str_detect(event_desc, "CITATION")
+      ),
+      reason_for_stop = tr_reason[`REASON FOR STOP`],
+      subject_race = tr_race[RACE],
+      search_conducted = tr_search[SEARCH],
+      contraband_found = tr_contraband[SEARCH]
+    ) %>%
+    add_lat_lng(
+      "incident_location",
+      calculated_features_path
+    ) %>%
+    standardize(d$metadata)
+}
---
+++ lib/states/tx/arlington.R
@@ -0,0 +1,73 @@
+source("common.R")
+
+load_raw <- function(raw_data_dir, n_max) {
+  data <- tibble()
+  loading_problems <- list()
+  r <- function(fname) {
+    tbl <- read_csv(file.path(raw_data_dir, fname))
+    loading_problems[[fname]] <<- problems(tbl)
+    tbl
+  }
+  subject <- r("orr_53698_2016_subject_stops.csv")
+  traffic <- r("orr_53698_2016_traffic_stops.csv")
+  subject["incident_type"] = "pedestrian"
+  traffic["incident_type"] = "vehicular"
+  data <- bind_rows(subject, traffic)
+  if (nrow(data) > n_max) {
+    data <- data[1:n_max, ]
+    break
+  }
+  bundle_raw(data, loading_problems)
+}
+
+
+clean <- function(d, calculated_features_path) {
+
+  tr_race <- c(
+    "W" = "white",
+    "B" = "black",
+    "H" = "hispanic",
+    "A" = "asian/pacific islander",
+    "O" = "other/unknown",
+    "D" = "other/unknown",
+    "C" = "other/unknown",
+    "X" = "other/unknown",
+    "I" = "other/unknown",
+    "K" = "other/unknown",
+    "L" = "other/unknown",
+    "E" = "other/unknown",
+    "P" = "other/unknown",
+    "F" = "other/unknown"
+  )
+
+  # TODO(phoebe): what are PRA, xCoordinate, yCoordinate?
+  # https://app.asana.com/0/456927885748233/653410849000224
+  d$data %>%
+    rename(
+      officer_id = `PrimaryID (Officer ID)`,
+      precinct = District,
+      beat = Beat
+    ) %>%
+    mutate(
+      subject_race = tr_race[`1st digit (Race)`],
+      subject_sex = tr_sex[`2nd digit (Gender)`],
+      # TODO(phoebe): can we get a data dictionary for this? X, N, I?
+      # https://app.asana.com/0/456927885748233/653410849000225
+      reason_for_stop = `3rd digit (Reason for Stop)`,
+      # TODO(phoebe): can we get a data dictionary for this? R, C, K, L?
+      # https://app.asana.com/0/456927885748233/653410849000225
+      incident_outcome = `4th digit (Final Outcome)`,
+      # TODO(phoebe): can we get a data dictionary for 6th digit (Search
+      # Outcome?)
+      # https://app.asana.com/0/456927885748233/653410849000225
+      search_conducted = !is.na(`6th digit (Search Outcome)`),
+      incident_date = parse_date(InitiateDate),
+      incident_time = seconds_to_hms(InitiateTime),
+      incident_location = coalesce(Address1, Address2)
+    ) %>%
+    add_lat_lng(
+      "incident_location",
+      calculated_features_path
+    ) %>%
+    standardize(d$metadata)
+}
