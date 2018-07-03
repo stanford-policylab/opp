1d1f926edb32a6d5ffeaabfa573eb7f3de5528a6
Joe Nudell <jnu@stanford.edu>
2018-07-03 08:07:38 -0700

    wy - statewide

---
+++ lib/states/wy/statewide.R
@@ -0,0 +1,64 @@
+source("common.R")
+
+load_raw <- function(raw_data_dir, n_max) {
+  wy2011 <- load_regex(raw_data_dir, ".*2011.csv", n_max = n_max)
+  wy2012_1 <- load_single_file(raw_data_dir, "jan-june2012.csv", n_max = n_max)
+  # NOTE: Second 2012 file is missing `trci_id` column but is otherwise the same.
+  wy2012_2 <- load_single_file(raw_data_dir, "jul-dec2012.csv", n_max = n_max)
+  bind_rows(
+      wy2011$data,
+      wy2012_1$data,
+      wy2012_2$data
+    ) %>%
+    select(
+      # NOTE: There are lots of empty columns from the spreadsheet conversion
+      # that we drop here.
+      -starts_with("X")
+    ) %>%
+    bundle_raw(
+      c(wy2011$loading_problems, wy2012_1$loading_problems, wy2012_2$loading_problems)
+    )
+}
+
+
+clean <- function(d, helpers) {
+
+  # TODO(phoebe): can we get reason_for_stop/search/contraband fields?
+  # https://app.asana.com/0/456927885748233/731173686918279
+  d$data %>%
+    # NOTE: Each row represents an individual event in a stop. The following
+    # grouping will get us to the stop level.
+    group_by(
+      tc_date,
+      tc_time,
+      offcr_id,
+      emdivision,
+      streetnbr,
+      street,
+      city
+    ) %>%
+    # Take the first row for any stop. We will drop some violations doing this.
+    slice(
+      1
+    ) %>%
+    ungroup(
+    ) %>%
+    rename(
+      officer_id = offcr_id,
+      violation = statute
+    ) %>%
+    mutate(
+      date = parse_date(tc_date, "%Y/%m/%d"),
+      time = parse_time(tc_time, "%H%M"),
+      # NOTE: `city` column actually holds county
+      location = str_c_na(street, streetnbr, city),
+      county_name = city,
+      precinct = emdivision,
+      subject_age = as.integer(age),
+      subject_race = tr_race[race],
+      subject_sex = tr_sex[sex],
+      # NOTE: All stops in data are vehicle stops.
+      type = "vehicular"
+    ) %>%
+    standardize(d$metadata)
+}
