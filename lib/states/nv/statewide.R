6bb71e42e8cdeac178fa9b20fcb8d6feb8e072eb
Joe Nudell <jnu@stanford.edu>
2018-07-19 14:21:48 -0700

    cr fixes

--- lib/states/nv/statewide.R
+++ lib/states/nv/statewide.R
@@ -1,36 +1,23 @@
 source("common.R")
 
 load_raw <- function(raw_data_dir, n_max) {
-  d1 <- load_single_file(
-    raw_data_dir,
-    "copy_of_citationwoarrestfirsthalf.csv",
-    n_max = n_max
-  )
-  d2 <- load_single_file(
-    raw_data_dir,
-    "copy_of_citationswoarrestsecondhalf.csv",
-    n_max = n_max
-  )
-  d3 <- load_single_file(
-    raw_data_dir,
-    "copy_of_citationswitharrest_part3.csv",
-    n_max = n_max
-  )
-  bind_rows(
-    d1$data %>% rename(Offense = `Offense Description`),
-    d2$data %>% rename(Offense = `Offense Description`),
-    # NOTE: Add a Result field for consistency with other sources; we will
-    # normalize this later.
-    d3$data %>%
-      rename(
-        Code = `Offense Code`
-      ) %>%
-      mutate(
-        Result = 'ARREST'
-      ) %>%
-      select(-Arrest)
+  d <- load_regex(raw_data_dir, "citation", n_max = n_max)
+
+  mutate(
+    d$data,
+    # NOTE: Fix some column name inconsistencies between files.
+    Offense = coalesce(Offense, `Offense Description`),
+    Code = coalesce(Code, `Offense Code`),
+    # NOTE: The arrest file does not contain a Result column, so fill it in.
+    Result = coalesce(Result, if_else(Arrest == 'YES', 'ARREST', NA_character_))
   ) %>%
-  bundle_raw(c(d1$loading_problems, d2$loading_problems, d3$loading_problems))
+  # NOTE: Drop partial columns.
+  select(
+    -Arrest,
+    -`Offense Description`,
+    -`Offense Code`
+  ) %>%
+  bundle_raw(d$loading_problems)
 }
 
 
@@ -49,7 +36,8 @@ clean <- function(d, helpers) {
   # https://app.asana.com/0/456927885748233/749213148338105
   d$data %>%
     rename(
-      violation = Code
+      violation = Code,
+      subject_age = Age
     ) %>%
     mutate(
       date = parse_date(Date, "%Y/%m/%d"),
@@ -57,10 +45,6 @@ clean <- function(d, helpers) {
       type = "vehicular",
       location = NA,
       subject_race = tr_race[Race],
-      # NOTE: Age column contains a lot of negative values. Remove those as
-      # they are obviously wrong; there is likely still some garbage with the
-      # child ages.
-      subject_age = if_else(as.integer(Age) < 1, NA_integer_, as.integer(Age)),
       warning_issued = Result == "WARNING",
       citation_issued = Result == "CITATION",
       arrest_made = Result == "ARREST",
