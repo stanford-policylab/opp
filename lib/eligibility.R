91af7a12dd0e35e2a958419a0aad7fe3d98875e1
Daniel Jenson <djenson3@gatech.edu>
2018-09-07 17:31:33 -0700

    progress

---
+++ lib/eligibility.R
@@ -0,0 +1,21 @@
+source("opp.R")
+
+
+eligiblity <- function() {
+  paths <- opp_data_paths()
+  pmap
+}
+
+basic_stats_eligibility <- function() {
+}
+
+outcome_test_eligibility <- function() {
+}
+
+threshold_test_eligibility <- function() {
+}
+
+veil_of_darkness_eligibility <- function() {
+}
+
+
