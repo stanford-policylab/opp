736fa7944fdddf030f9720fb2553a7e758e05cfe
Daniel Jenson <Daniel Jenson>
2018-09-10 16:32:53 -0700

    progress

--- lib/outcome_test.R
+++ lib/outcome_test.R
@@ -1,39 +1,46 @@
 source('opp.R')
 
-outcome_test <- function(state, city, control_for = c()) {
-  null_rate_warning_level = 0.05
 
+outcome_test <- function(
+  state,
+  city,
+  race_col = "subject_race",
+  search_col = "search_conducted",
+  contraband_col = "contraband_found",
+  control_for = c()
+) {
   d <- opp_load_data(state, city)
-  n <- nrow(d)
+  metadata <- list()
 
-  if (!is.element("subject_race", control_for)) {
-    control_for <- c("subject_race", control_for)
-  }
-  required_cols <- c(
-    control_for,
-    "search_conducted",
-    "contraband_found"
-  )
+  outcome_by <- c(race_col, control_for)
+  required_cols <- c(outcome_by, search_col, contraband_col)
   missing_cols <- setdiff(required_cols, colnames(d))
   if (length(missing_cols) > 0) {
     warning(str_c("missing columns: ", str_c(missing_cols, collapse = ", ")))
     return(NA)
   }
 
+  n <- nrow(d)
   d <- select_(d, .dots=required_cols) %>% drop_na()
   null_rate <- (n - nrow(d)) / n
-  if (null_rate > null_rate_warning_level) {
+  metadata['null_rate'] <- null_rate
+  if (null_rate > 0) {
     warning("at least one column null rate: ", pretty_percent(null_rate))
   }
 
-  filter(
+  results <- filter(
     d,
     search_conducted
   ) %>%
   group_by_(
-    .dots = control_for
+    .dots = outcome_by
   ) %>%
   summarize(
     contraband_found_rate = sum(contraband_found) / n()
+  )
+
+  list(
+    results = results,
+    metadata = metadata
   )
 }
