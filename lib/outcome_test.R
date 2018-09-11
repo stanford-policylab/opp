371c9b0405049fdb34388af4a54c7eff07b4f228
Daniel Jenson <Daniel Jenson>
2018-09-11 11:34:02 -0700

    progress

--- lib/outcome_test.R
+++ lib/outcome_test.R
@@ -1,6 +1,29 @@
 source('opp.R')
 
 
+#' Outcome Test
+#'
+#' @param tbl a tibble containing the following data
+#' @param ... additional attributes to control for when conducting outcome test
+#' @param demographic_col contains a population division of interest, i.e. race,
+#'        age group, sex, etc...
+#' @param action_col identifies the risk population, i.e. those who were
+#'        searched, frisked, summoned, etc...
+#' @param outcome_col contains the results of action specified by
+#'        \code{action_col}, i.e. if a search was conducted, an outcome
+#'        might be whether contraband was found
+#' 
+#' @return list with \code{results} and \code{metadata} keys
+#'
+#' @examples
+#' outcome_test(tbl, precinct)
+#' outcome_test(tbl, precinct, subject_race, search_conducted, contraband_found)
+#' outcome_test(
+#'   tbl,
+#'   demographic_col = subject_race,
+#'   action_col = search_conducted,
+#'   outcome_col = contraband_found
+#' )
 outcome_test <- function(
   tbl,
   ...,
