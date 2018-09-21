41359a63e30b1c34f04f178ac6b8ec4888364188
Amy Shoemaker <amyshoe@stanford.edu>
2018-09-20 21:18:56 -0700

    revised with dans comments

--- lib/disparity_plot.R
+++ lib/disparity_plot.R
@@ -6,18 +6,23 @@ library(tidyverse)
 #' @param ... additional attributes controled for when rates were computed
 #' @param demographic_col contains a population division of interest, i.e. race,
 #'        age group, sex, etc...
-#' @param dominant_demographic identifies the demographic to compare all others to
+#' @param majority_demographic identifies the demographic to compare all others to
 #' @param rate_col contains the rates to be compared between groups
 #' @param size_col contains the denominator of \code{rate_col}
+#' @param title plot title
+#' @param axis_title x-axis = \code{majority_demographic} \code{axis_title},
+#'        y-axis = "Minority" \code{axis_title}
+#' @param size_title title for point-size legend
+#' @param epsilon_rate sets margins on the min and max rate to expand axes
 #' 
-#' @return ggplot
+#' @return ggplot scatterplot of rates faceted by minority demographics
 #'
 #' @examples
 #' outcome_test(
 #'   tbl, 
 #'   precinct, 
 #'   demographic_col = subject_race, 
-#'   dominant_demographic = "white", 
+#'   majority_demographic = "white", 
 #'   rate_col = `contraband_found where search_conducted`,
 #'   size_col = n_search_conducted
 #'  )
@@ -25,7 +30,7 @@ plot_rates <- function(
   tbl,
   ...,
   demographic_col = subject_race, 
-  dominant_demographic = "white", 
+  majority_demographic = "white", 
   rate_col = `contraband_found where search_conducted`,
   size_col = n_search_conducted,
   title = "Contraband recovery rates by precinct",
@@ -40,69 +45,53 @@ plot_rates <- function(
   size_colq <- enquo(size_col)
   control_colnames <- colnames(select(tbl, !!!control_colqs)) 
   size_colname <- quo_name(size_colq)
-  minority_demographics <- setdiff(
+  minority_demographics_colnames <- setdiff(
     pull(tbl, !!demographic_colq),
-    dominant_demographic
+    majority_demographic
   )
   
-  rates_of_dominant_and_minority <- 
+  majority_and_minority_rates <-
     tbl %>%
     select(-!!size_colq) %>%
-    spread(
-      key = !!demographic_colq, 
-      value = !!rate_colq, 
-      fill = 0
-    ) %>%
-    rename(dominant_rate = dominant_demographic) %>%
-    gather(
-      key = minority_demographics_col, 
-      value = minority_rate, 
-      minority_demographics
-    )
+    spread(!!demographic_colq, !!rate_colq, fill = 0) %>%
+    rename(majority_rate = majority_demographic) %>%
+    gather(minority_demographic, minority_rate, minority_demographics_colnames)
   
-  size_of_dominant_plus_minority <-
+  majority_plus_minority_sizes <-
     tbl %>%
     select(-!!rate_colq) %>% 
-    spread(
-      key = !!demographic_colq, 
-      value = !!size_colq, 
-      fill = 0
-    ) %>%
-    rename(dominant_size = dominant_demographic) %>% 
-    gather(
-      key = minority_demographics_col, 
-      value = minority_size, 
-      minority_demographics
-    ) %>% 
-    group_by(!!!control_colqs, minority_demographics_col) %>% 
-    mutate(!!size_colname := dominant_size + minority_size) %>% 
+    spread(!!demographic_colq, !!size_colq, fill = 0) %>%
+    rename(majority_size = majority_demographic) %>% 
+    gather(minority_demographic, minority_size, minority_demographics_colnames) %>% 
+    group_by(!!!control_colqs, minority_demographic) %>% 
+    mutate(!!size_colname := majority_size + minority_size) %>% 
     ungroup() %>% 
-    select(!!size_colq, !!!control_colqs, minority_demographics_col)
+    select(!!size_colq, !!!control_colqs, minority_demographic)
   
   data <- left_join(
-    rates_of_dominant_and_minority,
-    size_of_dominant_plus_minority,
-    by = c(control_colnames, "minority_demographics_col")
+    majority_and_minority_rates,
+    majority_plus_minority_sizes,
+    by = c(control_colnames, "minority_demographic")
   )
   
-  axis_lims <- c(
+  axis_limits <- c(
     min(pull(tbl, !!rate_colq)) - epsilon_rate, 
     max(pull(tbl, !!rate_colq)) + epsilon_rate
   )
 
   data %>%
-    ggplot(aes_string("dominant_rate", "minority_rate")) +
+    ggplot(aes_string("majority_rate", "minority_rate")) +
     geom_point(aes_string(size = size_colname), shape = 1, alpha = 0.75) +
     geom_abline(linetype = "dashed") +
-    facet_grid(cols = vars(minority_demographics_col)) +
+    facet_grid(cols = vars(minority_demographic)) +
     theme_bw() +
     theme(panel.spacing = unit(1.0, "lines")) +
-    scale_x_continuous(labels = scales::percent, limits = axis_lims) +
-    scale_y_continuous(labels = scales::percent, limits = axis_lims) +
+    scale_x_continuous(labels = scales::percent, limits = axis_limits) +
+    scale_y_continuous(labels = scales::percent, limits = axis_limits) +
     scale_size_area(labels = scales::comma) +
     coord_fixed() +
     labs(
-      x = str_c(str_to_title(dominant_demographic), " ", axis_title),
+      x = str_c(str_to_title(majority_demographic), " ", axis_title),
       y = str_c("Minority ", axis_title),
       size = size_title,
       title = title
--- lib/threshold_test.R
+++ lib/threshold_test.R
@@ -3,7 +3,8 @@ library(parallel)
 library(rstan)
 library(tidyverse)
 
-source(here::here("lib", "disparity_plot.R"))
+source("~/opp/lib/disparity_plot.R")
+# source(here::here("lib", "disparity_plot.R"))
 
 #' Threshold Test
 #'
@@ -41,6 +42,7 @@ threshold_test <- function(
   demographic_col = subject_race,
   action_col = search_conducted,
   outcome_col = contraband_found,
+  majority_demographic = "white",
   n_iter = 5000,
   n_cores = min(5, parallel::detectCores() / 2)
 ) {
@@ -65,18 +67,16 @@ threshold_test <- function(
   n_no_nas <- nrow(tbl)
   tbl <-
     tbl %>% 
-    filter(
-      # remove inconsistent data where an outcome was recorded but there
-      # was no action taken
-      !(!!outcome_colq & !(!!action_colq))
-    )
+    # remove inconsistent data where an outcome was recorded but there
+    # was no action taken
+    filter(!(!!outcome_colq & !(!!action_colq)))
   
   metadata <- list()
   
   null_rate <- (n - n_no_nas) / n
   metadata['null_rate'] <- null_rate
   if (null_rate > 0) { 
-    issue_rate_warning(
+    rate_warning(
       null_rate,
       "was null and removed"
     )
@@ -85,7 +85,7 @@ threshold_test <- function(
   outcome_without_action_rate <- (n_no_nas - nrow(tbl)) / n
   metadata['outcome_without_action_rate'] <- outcome_without_action_rate
   if (outcome_without_action_rate > 0) {
-    issue_rate_warning(
+    rate_warning(
       outcome_without_action_rate,
       "was removed due to inconsistency: outcome was recorded but no action was taken"
     )
@@ -93,21 +93,15 @@ threshold_test <- function(
 
   data_summary <- 
     tbl %>% 
-    group_by(
-      !!demographic_colq,
-      !!!control_colqs
-    ) %>%
+    group_by(!!demographic_colq, !!!control_colqs) %>%
     summarize(
       n = n(),
       n_action = sum(!!action_colq),
       n_outcome = sum(!!outcome_colq)
     ) %>% 
-    ungroup(
-    ) %>% 
-    unite(
-      controls,
-      !!!control_colqs
-    ) %>% 
+    ungroup() %>% 
+    unite(controls, !!!control_colqs) %>% 
+    # NOTE: keep original column values to map stan output to values
     mutate(
       original_control = controls,
       original_demographic = !!demographic_colq
@@ -129,31 +123,28 @@ threshold_test <- function(
   )
   
   fit <- stan_threshold_test(stan_data, n_iter, n_cores)
-  post <- rstan::extract(fit)
+  posteriors <- rstan::extract(fit)
   
   summary_stats <- 
-    collect_avg_threshold_summary_stats(
+    collect_avg_threshold_test_summary_stats(
       data_summary, 
       !!demographic_colq,
-      dominant_demographic
+      majority_demographic,
+      posteriors
     )
     
   thresholds_by_group <- 
     data_summary %>%
     mutate(
-      thresholds = colMeans(signal_to_p(
-        post$threshold, 
-        post$phi, 
-        post$lambda
+      thresholds = colMeans(signal_to_percent(
+        posteriors$threshold, 
+        posteriors$phi, 
+        posteriors$lambda
       ))
     ) %>%
-    group_by(
-      controls
-    ) %>%
-    mutate(
-      # e.g., number of stops across all races in each district
-      total_group_count = sum(n) 
-    ) %>%
+    group_by(controls) %>%
+    # e.g., number of stops across all races in each district
+    mutate(total_group_count = sum(n)) %>%
     ungroup() %>% 
     select(
       original_demographic, 
@@ -167,7 +158,7 @@ threshold_test <- function(
       thresholds_by_group,
       original_control,
       demographic_col = original_demographic, 
-      dominant_demographic = dominant_demographic, 
+      majority_demographic = majority_demographic, 
       rate_col = thresholds,
       size_col = n_action,
       title = "Thresholds by division",
@@ -182,9 +173,141 @@ threshold_test <- function(
     results = list(
       thresholds_by_group = thresholds_by_group,
       aggregate_thresholds = summary_stats,
-      scatterplot_dominant_vs_minority = plot
+      scatterplot_majority_vs_minority = plot
     )
   )
+}
+
+
+# Creates a warning indicating that some percent of the data had some property
+rate_warning <- function(rate, message) {
+  warning(
+    str_c(
+      formatC(100 * rate, format = "f", digits = 2), 
+      "% of the data ",
+      message
+    ),
+    call. = FALSE
+  )
+}
+
+# converts the threshold signal into a percent value (0, 1)
+signal_to_percent <- function(x, phi, lambda){
+  phi * dnorm(x, lambda, 1) / 
+    (phi * dnorm(x, lambda, 1) + (1 - phi) * dnorm(x, 0, 1))
+}
+
+collect_avg_threshold_test_summary_stats <- function(
+  summary,
+  demographic_col,
+  majority_demographic,
+  posteriors
+) {
+  demographic_colq <- enquo(demographic_col)
+  avg_thresh <- accumulateRowMeans(
+    t(signal_to_percent(
+      posteriors$threshold,
+      posteriors$phi,
+      posteriors$lambda
+    )),
+    pull(summary, !!demographic_colq), 
+    summary$n
+  )
+  format_summary_stats(
+    summary, 
+    !!demographic_colq,
+    majority_demographic, 
+    avg_thresh
+  )
+} 
+
+# Matrix operation to perfrom accumulated row means over some sub-category i
+# @pre: length(i) == nrow(M)
+# @pre: length(w) == nrow(M)
+# e.g., 
+## if rows of @M represent race-district thresholds, and
+## if @i gives integer representation of which race each row in @M corresponds to,
+## then @output will be a matrix with by-race thresholds averaged across districts
+## averages are weighted according to @w (or, default is unweighted avg)
+# @post: ncol(output) == ncol(M)
+# @post: nrow(output) == n_distinct(i)
+accumulateRowMeans <- function(
+  M, # matrix to perform accumulated row means on
+  i, # lists which sub-category value each row in M corresponds to
+  w = rep(1, nrow(M)), # values used to create weightings for averaging
+  i_max = max(i)
+) {
+  weighted_indexer <- t(sapply(
+    1:i_max, 
+    function(j) { (i == j) * na_replace(w / sum(w[i == j]), 0) }
+  ))
+  weighted_indexer %*% M
+}
+
+na_replace <- function(x, r) if_else(is.finite(x), x, r)
+
+format_summary_stats <- function(
+  summary,
+  demographic_col,
+  majority_demographic,
+  avg_thresh
+) {
+  demographic_colq <- enquo(demographic_col)
+  majority_idx <-
+    summary %>% 
+    filter(original_demographic == majority_demographic) %>% 
+    pull(!!demographic_colq) %>% 
+    # extracts a single value
+    unique() 
+  
+  avg_diffs <- 
+    avg_thresh[-majority_idx,] -
+    matrix(
+      rep(avg_thresh[majority_idx,], nrow(avg_thresh) - 1),
+      nrow = nrow(avg_thresh) - 1
+    ) 
+  
+  tibble(
+    demographic = levels(summary$original_demographic),
+    avg_threshold = pretty_percent(rowMeans(avg_thresh)),
+    threshold_ci = format_confidence_interval(avg_thresh),
+    threshold_diff = append(
+      pretty_percent(rowMeans(avg_diffs), digits = 2), 
+      "", 
+      after = majority_idx - 1
+    ),
+    diff_ci = append(
+      format_confidence_interval(avg_diffs), 
+      "", 
+      after = majority_idx - 1
+    )
+  )
+}
+
+format_confidence_interval <- function(
+  # matrix over which confidence intervals are taken
+  M,
+  # dimension for which we're calculating CIs (defaults to rows; 2 = columns)
+  keep_dim = 1,
+  lower = 0.025, 
+  upper = 0.975,
+  digits = 2
+) {
+  apply(
+    matrixStats::rowQuantiles(M, probs = c(lower, upper)), 
+    keep_dim,
+    function(x) { 
+      str_c(
+        '(', 
+        str_flatten(pretty_percent(x, digits = digits), collapse = ', '), 
+        ')'
+      ) 
+    }
+  )
+}
+
+pretty_percent <- function(rate, digits = 1) {
+  str_c(formatC(100 * rate, format = "f", digits = digits), "%")
 }
 
 stan_threshold_test <- function(
@@ -201,7 +324,8 @@ stan_threshold_test <- function(
   n_iter_warmup <- min(2500, round(n_iter / 2))
   n_markov_chains <- 5
   nuts_max_tree_depth <- 12
-  path_to_stan_model <- here::here("stan", "threshold_test_2.stan")
+  # path_to_stan_model <- here::here("stan", "threshold_test_2.stan")
+  path_to_stan_model <- "~/opp/stan/threshold_test_2.stan"
   
   rstan::sampling(
     stan_model(path_to_stan_model),
@@ -217,116 +341,5 @@ stan_threshold_test <- function(
     iter = n_iter,
     refresh = n_iter_per_progress_update,
     warmup = n_iter_warmup
-  )
-}
-
-# Creates a warning indicating that some percent of the data had some property
-issue_rate_warning <- function(rate, message) {
-  warning(
-    str_c(
-      formatC(100 * rate, format = "f", digits = 2), 
-      "% of the data ",
-      message
-    ),
-    call. = FALSE
-  )
-}
-
-collect_avg_threshold_summary_stats <- function(
-  tt_summary, 
-  demographic_col,
-  dominant_demographic
-) {
-  
-  demographic_colq <- enquo(demographic_col)
-  
-  avg_thresh <- accumrowMeans(
-    t(signal_to_p(post$threshold, post$phi, post$lambda)),
-    pull(tt_summary, !!demographic_colq), 
-    tt_summary$n
-  )
-  
-  format_summary_stats(
-    tt_summary, 
-    !!demographic_colq,
-    dominant_demographic, 
-    avg_thresh
-  )
-} 
-
-# Matrix operation to perfrom accumulated row means over some sub-category i
-# @pre: length(i) == nrow(M)
-# @pre: length(w) == nrow(M)
-# e.g., 
-## if rows of @M represent race-district thresholds, and
-## if @i gives integer representation of which race each row in @M corresponds to,
-## then @output will be a matrix with by-race thresholds averaged across districts
-## averages are weighted according to @w (or, default is unweighted avg)
-# @post: ncol(output) == ncol(M)
-# @post: nrow(output) == n_distinct(i)
-accumrowMeans <- function(
-  M, # matrix to perform accumulated row means on
-  i, # lists which sub-category value each row in M corresponds to
-  w = rep(1, nrow(M)), # values used to create weightings for averaging
-  i_max = max(i)
-) {
-  weighted_indexer <- 
-    t(sapply(
-      1:i_max, 
-      function(j) { (i == j) * na_replace(w / sum(w[i == j]), 0) }
-    ))
-  
-  weighted_indexer %*% M
-}
-
-na_replace <- function(x, r) ifelse(is.finite(x), x, r)
-
-format_summary_stats <- function(
-  tt_summary,
-  demographic_col,
-  dominant_demographic,
-  avg_thresh
-) {
-  
-  demographic_colq <- enquo(demographic_col)
-  dominant_idx <- 
-    tt_summary %>% 
-    filter(original_demographic == dominant_demographic) %>% 
-    pull(!!demographic_colq) %>% 
-    unique()
-  
-  avg_diffs <- 
-    avg_thresh[-dominant_idx,] -
-    matrix(
-      rep(avg_thresh[dominant_idx,], nrow(avg_thresh) - 1),
-      nrow = nrow(avg_thresh) - 1
-    ) 
-
-  data.frame(
-    levels(tt_summary$original_demographic),
-    sprintf('%.3f', rowMeans(avg_thresh)),
-    format_confidence_interval(avg_thresh),
-    append(sprintf('%.4f', rowMeans(avg_diffs)), "", after = dominant_idx - 1), 
-    append(format_confidence_interval(avg_diffs), "", after = dominant_idx - 1)
-  ) %>%
-  setNames(c(
-    'Driver Race', 
-    'Average Threshold', 
-    '95% CI', 
-    'Threshold Diff', 
-    '95% CI of diffs'
-  ))
-}
-
-format_confidence_interval <- function(
-  M, # matrix over which confidence intervals are taken
-  keep_dim = 1, # dimension for which we're calculating CIs (defaults to rows; 2 = columns)
-  lower = 0.025, 
-  upper = 0.975
-) {
-  apply(
-    matrixStats::rowQuantiles(M, probs = c(lower, upper)), 
-    keep_dim,
-    function(x) { paste0('(', paste0(sprintf('%.3f', x), collapse = ', '), ')') }
   )
 }
