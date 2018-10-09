d681e23c203a1bc40fc68b3f22c7008a6ae3cff6
Samantha Robertson <srobert4@soal-2.stanford.edu>
2018-10-09 11:26:54 -0700

    geographic filtering on lat/lng outliers

--- lib/veil_of_darkness_test.R
+++ lib/veil_of_darkness_test.R
@@ -41,23 +41,37 @@ veil_of_darkness_test <- function(
   
   n_sec_day <- 24 * 60 * 60
   
+  median_lat <-
+    median(tbl, !!latitude_colq, na.rm = TRUE)
+  
+  median_lng <-
+    median(tbl, !!longitude_colq, na.rm = TRUE)
+  
+  lat_lng_tol <- 0.25 #degrees
+  
   tbl_intertwilight <-
     tbl %>% 
-    drop_na(
-      !!demographic_colq,
-      !!date_colq,
-      !!time_colq,
-      !!latitude_colq,
-      !!longitude_colq
+    filter(
+      !is.na(!!demographic_colq),
+      !is.na(!!date_colq),
+      !is.na(!!time_colq),
+      !is.na(!!latitude_colq),
+      !is.na(!!longitude_colq),
+      # throw out outliers geographically - CHECK ROBUSTNESS
+      !!latitude_colq < median_lat + lat_lng_tol,
+      !!latitude_colq > median_lat - lat_lng_tol,
+      !!longitude_colq < median_lng + lat_lng_tol,
+      !!longitude_colq > median_lng - lat_lng_tol
     ) %>% 
     mutate(
-      sunset = hms::hms(get_sunset_frac(
+      # Add timezone
+      sunset = hms::hms(get_darktime(
         !!latitude_colq, 
         !!longitude_colq, 
         as.POSIXct(str_c(!!date_colq, !!time_colq))
       ) * n_sec_day),
       dark = time > sunset,
-      black = !!demographic_colq == "black"
+      black = (!!demographic_colq == "black")
     ) %>% 
     filter(
       time > min(sunset),
@@ -65,40 +79,53 @@ veil_of_darkness_test <- function(
     )
   
   proportion_black <-
-    df %>% 
-    mutate(black = !!demographic_colq == "black") %>% 
+    tbl_intertwilight %>% 
     count(black, dark) %>% 
     group_by(dark) %>% 
     mutate(prop = n / sum(n))
   
   k_unadj <- (prob_given_bd(proportion_black, TRUE, FALSE) * 
-          prob_given_bd(proportion_black, FALSE, TRUE)) / 
+                prob_given_bd(proportion_black, FALSE, TRUE)) / 
     (prob_given_bd(proportion_black, FALSE, FALSE) *
        prob_given_bd(proportion_black, TRUE, TRUE))
   
-  model_time_const <- 
-    tbl_intertwilight %>% 
-    glm(formula = black ~ dark + ns(time, df = 6), 
-        family = "binomial")
+  # model_time_const <- 
+  #   tbl_intertwilight %>% 
+  #   glm(formula = black ~ dark + ns(time, df = 6), 
+  #       family = "binomial")
+  # 
+  # model_time_varying <-
+  #   tbl_intertwilight %>% 
+  #   glm(formula = black ~ dark * ns(time, df = 6), 
+  #       family = "binomial")
+  # 
+  # model_district_adj <-
+  #   tbl_intertwilight %>% 
+  #   glm(formula = black ~ dark + ns(time, df = 6) + district, 
+  #       family = "binomial")
   
-  model_time_varying <-
-    tbl_intertwilight %>% 
-    glm(formula = black ~ dark * ns(time, df = 6), 
-        family = "binomial")
-  
-  model_district_adj <-
-    tbl_intertwilight %>% 
-    glm(formula = black ~ dark + ns(time, df = 6) + district, 
-        family = "binomial")
+  list(
+    data = tbl_intertwilight,
+    k = k_unadj
+  )
 }
 
 n_sec_day <- 24 * 60 * 60
 
 testsf_wsunset <-
   testsf %>% 
-  filter(!is.na(lat), !is.na(lng), !is.na(date), !is.na(time)) %>% 
+  filter(
+    !is.na(lat), 
+    !is.na(lng), 
+    !is.na(date), 
+    !is.na(time),
+    lat < median(testsf$lat, na.rm = TRUE) + 0.25,
+    lat > median(testsf$lat, na.rm = TRUE) - 0.25,
+    lng < median(testsf$lng, na.rm = TRUE) + 0.25,
+    lng > median(testsf$lng, na.rm = TRUE) - 0.25
+  ) %>% 
   mutate(
-    sunset = hms::hms(get_sunset_frac(
+    sunset = hms::hms(get_darktime(
       lat, 
       lng, 
       as.POSIXct(str_c(date, time))
@@ -137,24 +164,57 @@ prop <- testsf_wsunset %>%
   mutate(prop = n / sum(n))
 
 k <- (prop %>% filter(black == TRUE, dark == FALSE) %>% pull(prop) * 
-  prop %>% filter(black == FALSE, dark == TRUE) %>%  pull(prop)) / 
+        prop %>% filter(black == FALSE, dark == TRUE) %>%  pull(prop)) / 
   (prop %>% filter(black == FALSE, dark == FALSE) %>% pull(prop) *
      prop %>% filter(black == TRUE, dark == TRUE) %>% pull(prop))
 
-# Returns the sunset time at the given latitude and longitude
+# Returns the end of civil twilight at the given latitude and longitude
 # and date/time as a fraction of the day
-get_sunset_frac <- function(
+get_darktime <- function(
   lat, 
   lng, 
   datetime
 ) {
   crds <- as.matrix(data.frame(lng, lat))
-  maptools::sunriset(crds, datetime, direction = "sunset")
+  maptools::crepuscule(
+    crds, 
+    datetime, 
+    solarDep = 6, 
+    direction = "dusk"
+  )
 }
 
 # todo - generalize
-prob_given_bd <- function(data, black, dark) {
+prob_given_bd <- function(data, b, d) {
   data %>% 
-    filter(black == black, dark == dark) %>% 
+    filter(
+      black == b, 
+      dark == d
+    ) %>% 
     pull(prop)
 }
+
+testsf_wsunset %>% 
+  ggplot(aes(sunset)) +
+  geom_histogram(bins = 50)
+
+testsf_wsunset %>% 
+  ggplot(aes(date, sunset)) +
+  geom_point(size = 0.1)
+
+testsf_wsunset %>% 
+  filter(sunset < 0.5 * n_sec_day) %>% 
+  View()
+
+testsf_wsunset %>%
+  mutate(
+    hours_since_darkness = interval(sunset, time) / hours(1)
+  ) %>% 
+  filter(
+    hours_since_darkness < 4,
+    hours_since_darkness > -4
+  ) %>% 
+  ggplot(aes(time, hours_since_darkness)) +
+  geom_point()
+
+# Generate synthetic data to test
