fbffc95664a462ddad79c98c51d046557255c7b5
Daniel Jenson <daniel.a.jenson@gmail.com>
2018-10-15 11:18:02 -0700

    progress

--- lib/coverage_map.R
+++ lib/coverage_map.R
@@ -1,26 +1,52 @@
+library(readr)
+library(here)
 library(maps)
-state_names <- map(database = "state")
-state_coverage <- c(
+
+state_names <- map(database = "state")$names
+sufficient_data_states <- c(
   "arizona",
   "california",
+  "colorado",
   "connecticut",
-  "delaware",
   "florida",
-  "illinois"
+  "illinois",
   "maryland",
-  "massachusettes",
+  "massachusetts:main",
   "missouri",
   "montana",
+  "nebraska",
   "new jersey",
-  "north carolina",
+  "north carolina:main",
   "ohio",
   "rhode island",
   "south carolina",
   "texas",
-  "utah",
   "vermont",
-  "washington",
-  "wisconsin",
+  "washington:main",
+  "wisconsin"
+)
+insufficient_data_states <- c(
+  "alabama",
+  "iowa",
+  "michigan:north",
+  "michigan:south",
+  "nevada",
+  "new hampshire",
+  "north dakota",
+  "oregon",
+  "south dakota",
+  "tennessee",
+  "virginia:main",
   "wyoming"
 )
-map(database = "state", col = c("white", "cornflowerblue")[1 + (v %in% , proj="tetra", fill=T, namesonly=T)
+
+state_coverage <- c(sufficient_data_states, insufficient_data_states)
+city_coverage <- read_csv(here::here("data", "city_coverage_geocodes.csv"))
+pdf(here::here("plots", "coverage_map.pdf"))
+map( database = "state",
+  col = c("white", "lightblue3")[1 + (state_names %in% state_coverage)],
+  fill=T,
+  namesonly=T
+)
+points(city_coverage$lng, city_coverage$lat, col="red", pch=16)
+dev.off()
