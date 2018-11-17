ea1dd35d34f6c2ffd73f28c2e7e950911cb9e805
Daniel Jenson <Daniel Jenson>
2018-11-16 21:20:48 -0800

    progress

--- lib/marijuana_legalization.R
+++ lib/marijuana_legalization.R
@@ -3,7 +3,8 @@ source("opp.R")
 
 
 marijuana_legalization_analysis <- function() {
-  tbl <- load() %>% add_legalization_info()
+  tbl <- load()
+  srp <- compose_search_rates_plots(tbl)
   list(
     tables = (
       # TODO(danj): directionally, the coefficients are the same, but they
@@ -52,24 +53,19 @@ load <- function() {
     "VT", "Statewide",
     "WI", "Statewide"
   ) %>%
-  opp_load_all_data()
-}
-
-
-add_legalization_info <- function(tbl) {
-  tbl %>%
-    mutate(
-      # NOTE: default is WA's legalization date, which is 1 day earlier than CO
-      legalization_date = as.Date("2012-12-09"),
-      legalization_date = if_else(
-        state == "CO",
-        as.Date("2012-12-10"),
-        legalization_date
-      ),
-      is_before_legalization = date < legalization_date,
-      is_test = state %in% c("CA", "CO"),
-      is_treatment = is_test & !is_before_legalization
-    )
+  opp_load_all_data() %>%
+  mutate(
+    # NOTE: default is WA's legalization date, which is 1 day earlier than CO
+    legalization_date = as.Date("2012-12-09"),
+    legalization_date = if_else(
+      state == "CO",
+      as.Date("2012-12-10"),
+      legalization_date
+    ),
+    is_before_legalization = date < legalization_date,
+    is_test = state %in% c("CA", "CO"),
+    is_treatment = is_test & !is_before_legalization
+  )
 }
 
 
@@ -130,9 +126,117 @@ calculate_search_rate_difference_in_difference_coefficients <- function(tbl) {
 }
 
 
-compose_test_misdemeanor_and_search_rates_plot <- function(tbl) {
+compose_search_rate_plots <- function(tbl) {
 
+  tbl <- tbl %>%
+    filter(
+      type == "vehicular",
+      subject_race %in% c("black", "hispanic", "white"),
+      !is.na(date),
+      !is.na(subject_race)
+    ) %>%
+    select(
+      state,
+      date,
+      legalization_date,
+      is_before_legalization,
+      subject_race,
+      search_basis,
+      search_conducted
+    ) %>%
+    mutate(
+      is_eligible_search =
+        # NOTE: excludes consent and other (non-discretionary)
+        search_basis %in% c("k9", "plain view", "probable cause")
+    ) %>%
+    group_by(
+      state,
+      date,
+      legalization_date,
+      is_before_legalization,
+      subject_race
+    ) %>%
+    summarize(
+      n_eligible_searches = sum(is_eligible_search, na.rm = T),
+      n_stops_with_search_data = sum(!is.na(search_conducted))
+    ) %>%
+    ungroup()
+  tbl
+
+#   trendlines <- group_by(tbl, state) %>% do(compute_search_trendline(.))
+
+#   # NOTE: roll up to quarters and remove data around legalization quarter
+#   # to reduce noise
+#   tbl <- tbl %>%
+#     to_rates_by_quarter(n_eligible_searches, n_stops_with_search_data) %>%
+#     filter(quarter != as.Date("2012-11-15"))
+
+#   list(t = trendlines, tbl = tbl)
+}
+
+
+to_rates_by_quarter <- function(tbl, numerator_col, denominator_col) {
+  nq <- enquo(numerator_col)
+  dq <- enquo(denominator_col)
+  tbl <-
+    tbl %>%
+    mutate(
+      quarter = as.Date(str_c(
+        c("02", "05", "08", "11")[quarter(date)],
+        "-15-",
+        year(date)
+      ))
+    ) %>%
+    group_by(state, subject_race, quarter) %>%
+    summarize(rate = sum(!!nq) / sum(!!dq))
+}
+
+
+compute_search_trendline <- function(tbl) {
+  # TODO(danj): why not use a linear model to predict rates? These numbers
+  # are slightly different
+  # lm(
+  #   eligible_search_rate ~ subject_race + date,
+  #   mutate(tbl, eligible_search_rate = n_eligible_searches / n_eligible)
+  # )
+  fit <- function(tbl) {
+    glm(
+      # NOTE: (n_successes, n_failures) ~ X
+      # NOTE: date is interpreted numerically
+      cbind(n_eligible_searches, n_stops_with_search_data - n_eligible_searches)
+        ~ subject_race + date,
+      binomial,
+      tbl
+    )
+  }
+  m_before <- fit(filter(tbl, is_before_legalization))
+  m_after <- fit(filter(tbl, !is_before_legalization))
+  score <- function(model, tbl) { predict(model, tbl, type = "response") }
   tbl %>%
+    group_by(state, subject_race, is_before_legalization) %>%
+    filter(date == min(date) | date == max(date)) %>%
+    distinct() %>%
+    ungroup() %>%
+    mutate(
+      predicted_search_rate = if_else(
+        is_before_legalization,
+        score(m_before, .),
+        score(m_after, .)
+      )
+    ) %>%
+    select(
+      state,
+      subject_race,
+      date,
+      is_before_legalization,
+      predicted_search_rate
+    )
+}
+
+
+compose_test_misdemeanor_plots <- function(tbl) {
+
+  tbl <- tbl %>%
     filter(
       type == "vehicular",
       subject_race %in% c("black", "hispanic", "white"),
@@ -145,9 +249,7 @@ compose_test_misdemeanor_and_search_rates_plot <- function(tbl) {
       date,
       legalization_date,
       subject_race,
-      violation,
-      search_basis,
-      search_conducted
+      violation
     ) %>%
     mutate(
       violation = str_to_lower(violation),
@@ -159,10 +261,7 @@ compose_test_misdemeanor_and_search_rates_plot <- function(tbl) {
       is_marijuana_violation = if_else(
         state == "WA",
         str_detect(violation, "drugs - misdemeanor"),
-        is_marijuana_violation,
-      is_eligible_search =
-        # NOTE: excludes consent and other (non-discretionary)
-        search_basis %in% c("k9", "plain view", "probable cause")
+        is_marijuana_violation
       )
     ) %>%
     group_by(
@@ -173,91 +272,11 @@ compose_test_misdemeanor_and_search_rates_plot <- function(tbl) {
       subject_race
     ) %>%
     summarize(
-      n_stops_with_search_data = sum(!is.na(search_conducted)),
-      n_eligible_searches = sum(is_eligible_search, na.rm = T),
-      n_stops_with_violation_data = sum(!is.na(violation)),
-      n_marijuana_violations = sum(is_marijuana_violation, na.rm = T)
+      n_marijuana_violations = sum(is_marijuana_violation, na.rm = T),
+      n_stops_with_violation_data = sum(!is.na(violation))
     )
-
-  trendlines <- group_by(tbl, state) %>% do(compute_search_trendline)
-
-  # NOTE: roll up to quarters and remove data around legalization quarter
-  # to reduce noise
-  tbl <-
-    tbl %>%
-    mutate(
-      quarter = as.Date(str_c(
-        c("02", "05", "08", "11")[quarter(date)],
-        "-15-",
-        year(date)
-      ))
-    ) %>%
-    filter(
-      # NOTE: this quarter includes the legalization dates for both CO and WA
-      quarter != as.Date("2012-11-15")
-    ) %>%
-    group_by(
-      state,
-      subject_race,
-      quarter
-    ) %>%
-    summarize(
-      n_stops_with_search_data = sum(n_stops_with_search_data),
-      n_eligible_searches = sum(n_eligible_searches),
-      eligible_search_rate = n_eligible_searches / n_stops_with_search_data,
-      n_stops_with_violation_data = sum(n_stops_with_violation_data),
-      n_marijuana_violations = sum(n_marijuana_violations),
-      marijuana_violation_rate =
-        n_marijuana_violations / n_stops_with_violation_data
-    )
-
-  ggplot(tbl, aes(x = quarter, y = ))
-
-
 
 }
-
-
-compute_search_trendline <- function(tbl) {
-  # TODO(danj): why not use a linear model to predict rates? These numbers
-  # are slightly different
-  # lm(
-  #   eligible_search_rate ~ subject_race + date,
-  #   mutate(tbl, eligible_search_rate = n_eligible_searches / n_eligible)
-  # )
-
-  # # NOTE: (n_successes, n_failures) ~ X
-  # # NOTE: date is interpreted numerically
-  fit <- function(tbl) {
-    glm(
-      cbind(n_eligible_searches, n_stops_with_search_data - n_eligible_searches)
-        ~ subject_race + date,
-      binomial,
-      tbl
-    )
-  }
-  m_before <- fit(filter(tbl, is_before_change))
-  m_after <- fit(filter(tbl, !is_before_change))
-  score <- function(model, tbl) { predict(model, tbl, type = "response") }
-  tbl %>%
-    group_by(state, city, subject_race, is_before_change) %>%
-    filter(date == min(date) | date == max(date)) %>%
-    distinct() %>%
-    ungroup() %>%
-    mutate(predicted_search_rate = if_else(
-      is_before_change,
-      score(m_before, .),
-      score(m_after, .)
-    )) %>%
-    select(
-      state,
-      city,
-      subject_race,
-      date,
-      is_before_change,
-      predicted_search_rate
-    )
-  }
 
 
 if (!interactive()) {
