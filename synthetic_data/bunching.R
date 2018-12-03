b76d51dfdc6c2853f8abdffaf1a695ac55ee5ca5
Daniel Jenson <Daniel Jenson>
2018-12-03 14:12:08 -0800

    progress

--- synthetic_data/bunching.R
+++ synthetic_data/bunching.R
@@ -2,43 +2,86 @@
 library(tidyverse)
 library(here)
 library(fs)
+library(boot)
 
 
-main <- function() {
-  set.seed(0)
-  n_majority <- 500
-  n_minority <- 500
-  n <- n_majority + n_minority
-  majority_lambda <- 5
-  minority_lambda <- 6
-  discount <- function(true_excess_bunching_speed, is_majority, leniency) {
-    if_else(
-      rnorm(1) <= 1 / true_excess_bunching_speed + 0.1 * is_majority
+generate <- function(
+  n_officers = 100,
+  n_stops_per_officer = 100,
+  # NOTE: used for generating what proportion of stops are miniorities
+  mean_proportion_minority = 0.3,
+  sd_proportion_minority = 0.1,
+  mean_leniency = 0.0,
+  sd_leniency = 0.5,
+  # NOTE: lambda is center of distribution above bunching speed
+  minority_lambda = 5,
+  majority_lambda = 5,
+  intercept = 0.05,
+  beta_bunching_excess_speed = -0.02,
+  beta_leniency = 1.0,
+  beta_majority = 0.05,
+  seed = 0
+) {
+  set.seed(seed)
+
+  pr_discount <- function(bunching_excess_speed, leniency, is_majority) {
+    inv.logit(
+      intercept
+      + beta_bunching_excess_speed * bunching_excess_speed
+      + beta_leniency * leniency
+      + beta_majority * is_majority
     )
   }
-  tbl <- tibble(
-    is_majority = c(
-      rep(T, n_majority),
-      rep(F, n_minority),
+  
+  tibble(
+    officer_id = seq(1:n_officers),
+    leniency = positive_normal(
+      n_officers,
+      mean_leniency,
+      sd_leniency
     ),
-    true_excess_bunching_speed = c(
-      dpois(n_majority, majority_lambda),
-      dpois(n_minority, minority_lambda)
+    p_minority = positive_normal(
+      n_officers,
+      mean_proportion_minority,
+      sd_proportion_minority
     )
-    leniency = dnorm(n, mean = 0, sd = 0.5),
-    leniency = if_else(leniency < 0, 0, leniency),
-    recorded_excess_bunching_speed <- discount(
-      true_excess_bunching_speed,
-      is_majority,
-      leniency 
+  ) %>%
+  repeat_rows(
+    n_stops_per_officer
+  ) %>%
+  mutate(
+    is_majority = if_else(runif(nrow(.)) > p_minority, 1, 0),
+    bunching_excess_speed = if_else(
+      is_majority > 0,
+      rpois(nrow(.), majority_lambda),
+      rpois(nrow(.), minority_lambda)
+    ),
+    recorded_excess_speed = if_else(
+      runif(nrow(.)) < pr_discount(
+        bunching_excess_speed,
+        leniency,
+        is_majority
+      ),
+      as.integer(0),
+      bunching_excess_speed
     )
   )
-    
-  output_dir <- dir_create(here::here("synthetic_data"))
-  write_csv(tbl, path(output_dir, "bunching.csv"))
+}
+
+
+repeat_rows <- function(tbl, n_each) {
+  tbl[rep(seq_len(nrow(tbl)), each=n_each), ]
+}
+
+
+positive_normal <- function(n, mean, sd) {
+  v <- rnorm(n, mean, sd)
+  if_else(v < 0, 0, v)
 }
 
 
 if (!interactive()) {
-  main()
+  tbl <- generate()
+  output_dir <- dir_create(here::here("synthetic_data"))
+  write_csv(tbl, path(output_dir, "bunching.csv"))
 }
