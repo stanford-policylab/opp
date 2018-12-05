4beff30eb7bbebd13a62d521ed26b0f54aee0d7f
Daniel Jenson <Daniel Jenson>
2018-12-05 15:09:14 -0800

    progress

--- synthetic_data/bunching.R
+++ synthetic_data/bunching.R
@@ -14,8 +14,8 @@ generate <- function(
   mean_leniency = 0.0,
   sd_leniency = 0.5,
   # NOTE: lambda is center of distribution above bunching speed
-  majority_lambda = 5,
-  minority_lambda = 6,
+  majority_lambda = 7,
+  minority_lambda = 7,
   # NOTE: exp(-1.5) ~ 0.22, meaning that conditional on nothing, the odds
   # of being discounted are roughly 1 in 5
   intercept = -1.5,
@@ -26,7 +26,7 @@ generate <- function(
   # NOTE: exp(1.0) ~ e ~ 2.7, so if the officer is fully lenient, the odds
   # of being discounted increase by ~3x; similarly, if the officer leniency
   # increases by 0.1, exp(0.1) ~ 1.11, meaning odds increase by ~11%
-  beta_leniency = 1.0,
+  beta_leniency = 2.0,
   # NOTE: exp(0.05) ~ 1.05, so if the driver is in the majority class,
   # the odds of being discounted increase by 5%
   beta_majority = 0.05,
@@ -61,39 +61,47 @@ generate <- function(
   #   )
   # }
   
-  tibble(
-    officer_id = seq(1:n_officers),
-    leniency = positive_normal(
-      n_officers,
-      mean_leniency,
-      sd_leniency
-    ),
-    p_minority = positive_normal(
-      n_officers,
-      mean_proportion_minority,
-      sd_proportion_minority
-    )
-  ) %>%
-  repeat_rows(
-    n_stops_per_officer
-  ) %>%
-  mutate(
-    is_majority = if_else(runif(nrow(.)) > p_minority, 1, 0),
-    bunching_excess_speed = if_else(
-      is_majority > 0,
-      rpois(nrow(.), majority_lambda),
-      rpois(nrow(.), minority_lambda)
-    ),
-    recorded_excess_speed = if_else(
-      runif(nrow(.)) < pr_discount(
-        bunching_excess_speed,
-        leniency,
-        is_majority
+  tbl <-
+    tibble(
+      officer_id = seq(1:n_officers),
+      leniency = positive_normal_truncate_to_zero(
+        n_officers,
+        mean_leniency,
+        sd_leniency
       ),
-      as.integer(0),
-      bunching_excess_speed
+      p_minority = positive_normal_truncate_to_zero(
+        n_officers,
+        mean_proportion_minority,
+        sd_proportion_minority
+      )
+    ) %>%
+    repeat_rows(
+      n_stops_per_officer
+    ) %>%
+    mutate(
+      is_majority = if_else(runif(nrow(.)) > p_minority, 1, 0),
+      bunching_excess_speed = if_else(
+        is_majority > 0,
+        rpois(nrow(.), majority_lambda),
+        rpois(nrow(.), minority_lambda)
+      ),
+      p_discount = pr_discount(bunching_excess_speed, leniency, is_majority),
+      recorded_excess_speed = if_else(
+        runif(nrow(.)) < p_discount,
+        as.integer(0),
+        bunching_excess_speed
+      )
     )
-  )
+
+}
+
+
+format_for_stan_bunching_aggregate <- function(tbl) {
+  tbl %>%
+    group_by(officer_id, leniency) %>%
+    summarize(leniency_estimate = mean(recorded_excess_speed == 0)) %>%
+    ungroup()
+  # TODO(danj): finish
 }
 
 
@@ -102,9 +110,9 @@ repeat_rows <- function(tbl, n_each) {
 }
 
 
-positive_normal <- function(n, mean, sd) {
+positive_normal_truncate_to_zero <- function(n, mean, sd) {
   v <- rnorm(n, mean, sd)
-  if_else(v < 0, 0, v)
+  if_else(v < 0 | v > 1, 0, v)
 }
 
 
