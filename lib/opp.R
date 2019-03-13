5014d7f35f39b8f11299a35bf8379165e56bbef2
Daniel Jenson <Daniel Jenson>
2019-03-12 17:40:06 -0700

    progress

--- lib/opp.R
+++ lib/opp.R
@@ -325,19 +325,19 @@ opp_data_paths <- function() {
 
 
 opp_download_all_data <- function() {
-  opp_apply(opp_download_data)
+  opp_apply(function(state, city) opp_download_data(state, city, load = F))
 }
 
 
 opp_download_analyses_data <- function() {
   opp_apply(
-    opp_download_data,
+    function(state, city) opp_download_data(state, city, load = F),
     opp_locations_used_in_analyses()
   )
 }
 
 
-opp_download_data <- function(state, city) {
+opp_download_data <- function(state, city, load = T) {
   if (!link_exists(here::here("data")))
     opp_set_download_directory("/tmp/opp_data")
   output_path <- opp_clean_data_path(state, city)
@@ -347,6 +347,8 @@ opp_download_data <- function(state, city) {
     url <- urls[str_detect(urls, "\\.rds")]
     download.file(url, output_path)
   }
+  if (load)
+    return(opp_load_clean_data(state, city))
 }
 
 
