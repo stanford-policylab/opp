e10c30128f9f5222b4839ab9c74c0c301194b575
Amy Shoemaker <amyshoe@stanford.edu>
2019-05-15 11:53:38 -0700

    Reprocess states

--- lib/states/oh/statewide.R
+++ lib/states/oh/statewide.R
@@ -2,15 +2,39 @@ source(here::here("lib", "common.R"))
 
 
 load_raw <- function(raw_data_dir, n_max) {
-  d <- load_all_csvs(raw_data_dir, n_max = n_max)
+  d <- load_regex(raw_data_dir, "^OSHP_", n_max = n_max)
+  d_2016_1 <- load_single_file(raw_data_dir, "cad2016.csv", n_max = n_max)
+  d_2016_2_to_2017 <- load_regex(raw_data_dir, "^premierone", n_max = n_max)
+  
+  # Actual split in 2016 appears to be 7/6, but there are some straggler
+  # dates with just a few stops
+  d$data <- bind_rows(
+    d$data, 
+    d_2016_1$data %>% 
+      filter(as.Date(mdy_hms(DATE_STAMP)) < ymd("2017-07-06")),
+    d_2016_2_to_2017$data %>% 
+      rename(
+        DATE_STAMP = IncidentDate,
+        TYPE = IncidentTypeCode
+      ) %>% 
+      rename_all(str_to_upper) %>% 
+      filter(as.Date(mdy_hms(DATE_STAMP)) >= ymd("2017-07-06"))
+  )
+  d$loading_problems <- c(
+    d$loading_problems, 
+    d_2016_1$loading_problems,
+    d_2016_2_to_2017$loading_problems
+  )
+  
   bundle_raw(d$data, d$loading_problems)
 }
-
 
 clean <- function(d, helpers) {
   tr_county <- json_to_tr(helpers$load_json("OH_counties.json"))
   tr_violations <- json_to_tr(helpers$load_json("OH_violations.json"))
-
+  # to accommodate 2017, take out periods in violation codes:
+  names(tr_violations) <- str_replace_all(names(tr_violations), "\\.", "")
+  
   tr_race <- c(
     "1" = "white",
     "2" = "black",
@@ -19,44 +43,69 @@ clean <- function(d, helpers) {
     "5" = "other/unknown",
     "6" = "other/unknown"
   )
-
-  violations_frame <- d$data %>%
-    select(
-      INCIDENT_PK,
-      ORC_STRING
-    ) %>%
+  
+  tr_race_raw <- c(
+    "1" = "white",
+    "2" = "black",
+    "3" = "hispanic",
+    "4" = "asian",
+    "5" = "native american",
+    "6" = "unknown"
+  )
+  
+  violations_df <- 
+    d$data %>% 
+    # violations pre 2017
+    filter(!is.na(ORC_STRING)) %>% 
+    mutate(
+      upper_orc_string = str_replace_all(str_to_upper(ORC_STRING), "\\.", "")
+    ) %>% 
+    bind_rows(
+      d$data %>%
+        # violations from 2017
+        filter(str_detect(DISPOSITIONS, "\\d{3,}")) %>% 
+        mutate(upper_orc_string = stri_join_list(
+          str_extract_all(str_to_upper(DISPOSITIONS), 
+                          "\\d{3,}[A-Z0-9]+"), 
+          sep = ",")
+        )
+    ) %>% 
     separate_rows(
-      ORC_STRING,
+      upper_orc_string,
       sep = ","
     ) %>%
-    mutate(
-      upper_orc_string = toupper(ORC_STRING),
-      violation = fast_tr(upper_orc_string, tr_violations),
-      violation = str_c_na(upper_orc_string, violation, sep = ": "),
-      violation = replace(violation, violation == "", NA_character_)
-    ) %>%
+    mutate(violation = fast_tr(upper_orc_string, tr_violations)) %>% 
     group_by(
-      INCIDENT_PK
-    ) %>%
+      DATE_STAMP, INCIDENT_PK, TYPE, ADDRESS, LATITUDE, LONGITUDE, COUNTY_CODE,
+      UNIT, EVENT_NBR, DISP_STRING, ORC_STRING, ASINC_STRING, DISPOSITIONS
+    ) %>% 
     summarize(
       violation = str_c_sort_uniq(violation)
-    ) %>%
+    ) %>% 
     ungroup()
 
   d$data %>%
+    left_join(violations_df) %>% 
     rename(
       location = ADDRESS,
       lat = LATITUDE,
       lng = LONGITUDE,
       officer_id = UNIT
     ) %>%
+    add_raw_colname_prefix(
+      DISP_STRING,
+      ORC_STRING, 
+      DISPOSITIONS
+    ) %>% 
     mutate(
       datetime = parse_datetime(DATE_STAMP, format = "%m/%d/%Y %H:%M:%S"),
       date = as.Date(datetime),
       time = format(datetime, "%H:%M:%S"),
       county_name = fast_tr(COUNTY_CODE, tr_county),
-      sex_and_race_codes = str_extract(DISP_STRING, "[1-6][FM]"),
+      disp_info = str_c(raw_DISP_STRING, raw_DISPOSITIONS, sep = "|"),
+      sex_and_race_codes = str_extract(disp_info, "[1-6][FM]"),
       subject_race = fast_tr(substr(sex_and_race_codes, 1, 1), tr_race),
+      raw_race = fast_tr(substr(sex_and_race_codes, 1, 1), tr_race_raw),
       subject_sex = fast_tr(substr(sex_and_race_codes, 2, 2), tr_sex),
       # NOTE: The data received in Dec 2015 and Feb 2016 are vehicular stops by
       # the Ohio State Highway Patrol.
@@ -65,24 +114,22 @@ clean <- function(d, helpers) {
       # NOTE: The following are the only disposition codes that clearly indicate
       # arrest or warning. Can't find disposition codes that clearly indicate a
       # citation.
-      arrest_made = str_detect(DISP_STRING, "75ARR|75OVI|75WAR|76WAR"),
-      warning_issued = str_detect(DISP_STRING, "WARN"),
+      arrest_made = str_detect(disp_info, "75ARR|75OVI|75WAR|76WAR"),
+      warning_issued = str_detect(disp_info, "WARN"),
       outcome = first_of(
         "arrest" = arrest_made,
         "warning" = warning_issued
       ),
-      contraband_drugs = str_detect(ORC_STRING, "2925"),
-      contraband_found = if_else(contraband_drugs, TRUE, NA),
-      search_conducted = str_detect(DISP_STRING, "24"),
+      # NOTE: 97 rows are NA; we consider these search F
+      search_conducted = str_detect(disp_info, "(^|\\,)24") & !is.na(disp_info),
       search_basis = first_of(
-        "k9" = str_detect(DISP_STRING, "24C?K"),
-        "consent" = str_detect(DISP_STRING, "24R"),
+        "k9" = str_detect(disp_info, "(^|\\,)24C?K"),
+        "consent" = str_detect(disp_info, "(^|\\,)24R"),
         "probable cause" = search_conducted
-      )
-    ) %>%
-    left_join(
-      violations_frame,
-      by = "INCIDENT_PK"
+      ),
+      contraband_drugs = str_detect(raw_ORC_STRING, "2925") | 
+        str_detect(disp_info, "2925"),
+      contraband_found = if_else(contraband_drugs, TRUE, NA)
     ) %>%
     standardize(d$metadata)
 }
--- lib/states/sc/statewide.R
+++ lib/states/sc/statewide.R
@@ -3,6 +3,9 @@ source(here::here("lib", "common.R"))
 
 load_raw <- function(raw_data_dir, n_max) {
   d <- load_all_csvs(raw_data_dir, n_max = n_max)
+  colnames(d$data) <- make_ergonomic(colnames(d$data))
+  # Add orig camel case to match calculated features of contraband classification
+  d$data <- mutate(d$data, ContrabandDesc = contrabanddesc)
   bundle_raw(d$data, d$loading_problems)
 }
 
@@ -27,33 +30,43 @@ clean <- function(d, helpers) {
   )
   
   d$data %>%
+    add_raw_colname_prefix(
+      officer_race,
+      race,
+      sex,
+      sectionnum, 
+      offensecode,
+      contact_type,
+      contrabanddesc
+    ) %>% 
     rename(
-      county_name = County,
-      subject_age = `Defendent Age`,
-      officer_id = `Officer Badge Number`,
-      officer_age = `Officer Age`,
-      officer_last_name = `Officer Name`,
-      department_id = PoliceDivision,
-      reason_for_stop = ContactReason
+      county_name = county,
+      subject_age = defendent_age,
+      officer_id = officer_badge_number,
+      officer_age = officer_age,
+      officer_last_name = officer_name,
+      department_id = policedivision,
+      reason_for_stop = contactreason
     ) %>%
     helpers$add_contraband_type(
       "ContrabandDesc"
     ) %>%
     mutate(
-      date = parse_date(DateIssued, "%m/%d/%Y"),
-      location = str_c_na(HighwayType, HighwayNum, sep = " "),
-      lat = parse_number(LatitudeDec),
+      date = parse_date(dateissued, "%m/%d/%Y"),
+      location = str_c_na(highwaytype, highwaynum, sep = " "),
+      county_name = str_c(county_name, " County"),
+      lat = parse_number(latitudedec),
       lat = if_else(lat == 0, NA_real_, lat),
-      lng = parse_number(LongitudeDec),
+      lng = parse_number(longitudedec),
       lng = if_else(lng == 0, NA_real_, lng),
-      subject_race = fast_tr(Race, tr_race),
-      subject_sex = fast_tr(Sex, tr_sex),
-      officer_race = fast_tr(`Officer Race`, tr_race),
+      subject_race = fast_tr(raw_race, tr_race),
+      subject_sex = fast_tr(raw_sex, tr_sex),
+      officer_race = fast_tr(raw_officer_race, tr_race),
       # NOTE: Data received in Aug 2016 was mostly vehicular stops.
       type = if_else(reason_for_stop == "Pedestrian", "pedestrian", "vehicular"),
-      violation = str_c_na(SectionNum, OffenseCode, sep = " "),
-      arrest_made = FelonyArrest == "1" | Jailed == "1",
-      citation_issued = `Contact Type` == "Citation",
+      violation = str_c_na(raw_sectionnum, raw_offensecode, sep = " "),
+      arrest_made = felonyarrest == "1" | jailed == "1",
+      citation_issued = raw_contact_type == "Citation",
       # TODO(phoebe): The other main value of "Contact Type" is "Public
       # Contact". It is unclear whether a warning is issued in the case of
       # "Public Contact". This needs to be clarified.
@@ -62,26 +75,31 @@ clean <- function(d, helpers) {
         "arrest" = arrest_made,
         "citation" = citation_issued
       ),
-      contraband_drugs = contraband_drugs == "1" | ContrabandDrugs == "1"
-        | ContrabandDrugParaphenalia == "1",
-      contraband_weapons = contraband_weapons == "1" | ContrabandWeapons == "1",
+      contraband_drugs = contraband_drugs == "1" | contrabanddrugs == "1"
+        | contrabanddrugparaphenalia == "1",
+      contraband_weapons = contraband_weapons == "1" | contrabandweapons == "1",
       # NOTE: Including `!is.na(ContrabandDesc)` was discussed at length. This
       # mimics the processing decision of the old analysis, in addition to staying
       # true to the department's definition of contraband. Without that inclusion,
       # contraband recovery is low enough to raise skepticism, and includes many,
       # many zeros, especially for hispanic recovery.
-      # TODO(amy): Seems like there are very few instances when ContrabandDesc,
+      # TODO: Seems like there are very few instances when ContrabandDesc,
       # which is a free field, indicates either no search was conducted or no
       # contraband was recovered, etc. Does not seem large enough that trends would
       # change, but may be worth in the future doing some work to extract contraband
       # vs no contraband from this ContrabandDesc field.
       contraband_found = contraband_drugs | contraband_weapons 
-        | Contraband == "1" | !is.na(ContrabandDesc),
-      search_person = SubjectSearched == "1" | PassengerSearched == "1",
-      search_vehicle = VehicleSearched == "1",
-      search_conducted = Searched == "1" | search_person | search_vehicle,
+        | contraband == "1" | !is.na(ContrabandDesc),
+      search_person = subjectsearched == "1" | passengersearched == "1",
+      search_vehicle = vehiclesearched == "1",
+      # NOTE: 4 "searched" are NA. We assume these to be False
+      search_conducted = !is.na(searched) & 
+        (searched == "1" | search_person | search_vehicle),
+      # An additional one row NA from search person/vehicle; cast to FALSE
+      search_conducted = replace_na(search_conducted, FALSE),
       # NOTE: mimics old opp
-      contraband_found = if_else(search_conducted & is.na(contraband_found), FALSE, contraband_found)
+      contraband_found = if_else(search_conducted & is.na(contraband_found), 
+                                 FALSE, contraband_found)
     ) %>%
     standardize(d$metadata)
 }
