42761f1c4a6d34eb24c01af9508ee9173c5064f4
Daniel Jenson <Daniel Jenson>
2020-01-24 14:59:36 -0800

    updating diff cis

--- lib/diff_cis.R
+++ lib/diff_cis.R
@@ -5,25 +5,25 @@ library(stringr)
 
 printf <- function(...) invisible(print(sprintf(...)))
 
-calculate_diff_cis <- function(rates, ses, ns) {
+calculate_diff_cis <- function(rates, ses) {
   z <- 1.96
   city_white_black_diff <- rates['city_white'] - rates['city_black']
-  city_white_black_se <- sqrt(ses['city_white']^2 / ns['city_white'] + ses['city_black']^2 / ns['city_black'])
+  city_white_black_se <- sqrt(ses['city_white']^2 + ses['city_black']^2)
   city_white_black_lower_ci <- city_white_black_diff - z * city_white_black_se
   city_white_black_upper_ci <- city_white_black_diff + z * city_white_black_se
 
   city_white_hispanic_diff <- rates['city_white'] - rates['city_hispanic']
-  city_white_hispanic_se <- sqrt(ses['city_white']^2 / ns['city_white'] + ses['city_hispanic']^2 / ns['city_hispanic'])
+  city_white_hispanic_se <- sqrt(ses['city_white']^2 + ses['city_hispanic']^2)
   city_white_hispanic_lower_ci <- city_white_hispanic_diff - z * city_white_hispanic_se
   city_white_hispanic_upper_ci <- city_white_hispanic_diff + z * city_white_hispanic_se
 
   state_white_black_diff <- rates['state_white'] - rates['state_black']
-  state_white_black_se <- sqrt(ses['state_white']^2 / ns['state_white'] + ses['state_black']^2 / ns['state_black'])
+  state_white_black_se <- sqrt(ses['state_white']^2 + ses['state_black']^2)
   state_white_black_lower_ci <- state_white_black_diff - z * state_white_black_se
   state_white_black_upper_ci <- state_white_black_diff + z * state_white_black_se
 
   state_white_hispanic_diff <- rates['state_white'] - rates['state_hispanic']
-  state_white_hispanic_se <- sqrt(ses['state_white']^2 / ns['state_white'] + ses['state_hispanic']^2 / ns['state_hispanic'])
+  state_white_hispanic_se <- sqrt(ses['state_white']^2 + ses['state_hispanic']^2)
   state_white_hispanic_lower_ci <- state_white_hispanic_diff - z * state_white_hispanic_se
   state_white_hispanic_upper_ci <- state_white_hispanic_diff + z * state_white_hispanic_se
 
@@ -68,7 +68,6 @@ group_by(agency, subject_race) %>%
 summarize(
   average_search_rate = mean(search_rate),
   std_error = sqrt(sum(var)) / n(),
-  n_locations = n(),
   lower_ci = average_search_rate - 1.96 * std_error,
   upper_ci = average_search_rate + 1.96 * std_error
 ) %>%
@@ -76,15 +75,13 @@ ungroup()
 
 rates <- pull(search_tbl, average_search_rate)
 ses <- pull(search_tbl, std_error)
-ns <- pull(search_tbl, n_locations)
 nms <- mutate(search_tbl, name = str_c(agency, "_", subject_race)) %>% pull()
 names(rates) <- nms
 names(ses) <- nms
-names(ns) <- nms
 
 
 print('Search Rates:')
-calculate_diff_cis(rates, ses, ns)
+calculate_diff_cis(rates, ses)
 
 disp <- read_rds(here::here("results", "disparity.rds"))
 
@@ -100,7 +97,6 @@ hit_rates <- bind_rows(
     agency = "city",
     hit_rate = mean(hit_rate),
     std_error = sqrt(sum(var)) / n(),
-    n_locations = n(),
     lower_ci = hit_rate - 1.96 * std_error,
     upper_ci = hit_rate + 1.96 * std_error
   ),
@@ -115,7 +111,6 @@ hit_rates <- bind_rows(
     agency = "state",
     hit_rate = mean(hit_rate),
     std_error = sqrt(sum(var)) / n(),
-    n_locations = n(),
     lower_ci = hit_rate - 1.96 * std_error,
     upper_ci = hit_rate + 1.96 * std_error
   )
@@ -123,12 +118,10 @@ hit_rates <- bind_rows(
 
 rates <- pull(hit_rates, hit_rate)
 ses <- pull(hit_rates, std_error)
-ns <- pull(hit_rates, n_locations)
 nms <- mutate(hit_rates, name = str_c(agency, "_", subject_race)) %>% pull()
 names(rates) <- nms
 names(ses) <- nms
-names(ns) <- nms
 
 
 print('Hit Rates:')
-calculate_diff_cis(rates, ses, ns)
+calculate_diff_cis(rates, ses)
