bedf10d4d2d819dd815a914bd4d8822e74b1f514
mgerchick <gerchick@soal-2.stanford.edu>
2020-09-08 10:59:24 -0700

    new chicago data progress

---
+++ lib/states/il/chicago_idot.R
@@ -0,0 +1,29 @@
+source(here::here("lib", "common.R"))
+
+# DATA_DIR <- "~/opp/data/states/il/chicago/raw_csv/"
+# 
+# # ISR 2020
+# isr <- read_csv("~/opp/data/states/il/chicago/raw_csv/15327-p580999-traffic-isr_sheet_2.csv")
+# 
+# # april 2018 to 
+# tsss_1 <- read_csv("~/opp/data/states/il/chicago/raw_csv/15327-p580999-traffic-isr_sheet_3.csv")
+# tsss_2 <- read_csv("~/opp/data/states/il/chicago/raw_csv/15327-p580999-traffic-isr_sheet_4.csv")
+
+load_raw <- function(raw_data_dir, n_max) {
+  tsss_1 <- load_single_file(
+    raw_data_dir,
+    "15327-p580999-traffic-isr_sheet_3.csv",
+    n_max
+  )
+  tsss_2 <- load_single_file(
+    raw_data_dir,
+    "15327-p580999-traffic-isr_sheet_4.csv",
+    n_max
+  ) 
+  
+  colnames(tsss_1$data) <- make_ergonomic(colnames(tsss_1$data))
+  colnames(tsss_2$data) <- make_ergonomic(colnames(tsss_2$data))
+  
+  union(tsss_1$data, tsss_2$data)  %>%
+    bundle_raw(c(tsss_1$loading_problems, tsss_2$loading_problems))
+}
